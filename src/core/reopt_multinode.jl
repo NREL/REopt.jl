33de261c15decc33466a1a3e804f1b522fbf30f1
Nick Laws <laws.nick@gmail.com>
2021-04-09 12:12:13 -0600

    add add_objective! method to reopt_multinode.jl

--- src/core/reopt_multinode.jl
+++ src/core/reopt_multinode.jl
@@ -279,10 +279,7 @@ function build_reopt!(m::JuMP.AbstractModel, ps::Array{REoptInputs})
 end
 
 
-function run_reopt(m::JuMP.AbstractModel, ps::Array{REoptInputs}; obj::Int=2)
-
-	build_reopt!(m, ps)
-
+function add_objective!(m::JuMP.AbstractModel, ps::Array{REoptInputs}; obj::Int=2)
 	if obj == 1
 		@objective(m, Min, sum(m[Symbol(string("Costs_", p.node))] for p in ps))
 	elseif obj == 2  # Keep SOC high
@@ -290,6 +287,15 @@ function run_reopt(m::JuMP.AbstractModel, ps::Array{REoptInputs}; obj::Int=2)
         - sum(sum(m[Symbol(string("dvStoredEnergy_", p.node))][:elec, ts] 
             for ts in p.time_steps) for p in ps) / (8760. / ps[1].hours_per_timestep))
 	end  # TODO need to handle different hours_per_timestep?
+	nothing
+end
+
+
+function run_reopt(m::JuMP.AbstractModel, ps::Array{REoptInputs}; obj::Int=2)
+
+	build_reopt!(m, ps)
+
+	add_objective!(m, ps; obj)
 
 	@info "Model built. Optimizing..."
 	tstart = time()
@@ -318,6 +324,7 @@ end
 
 
 function reopt_results(m::JuMP.AbstractModel, ps::Array{REoptInputs})
+	# TODO address Warning: The addition operator has been used on JuMP expressions a large number of times.
 	results = Dict{Union{Int, String}, Any}()
 	for p in ps
 		results[p.node] = reopt_results(m, p; _n=string("_", p.node))
