05347a7cd0fc66d47d155959ad9d8aa2d73cf52a
Nick Laws <nick.laws@nrel.gov>
2021-07-02 16:48:02 -0600

    Update test_with_cbc.jl

--- test/test_with_cbc.jl
+++ test/test_with_cbc.jl
@@ -71,7 +71,6 @@ end
            sum(results["Generator"]["year_one_to_load_series_kw"][i] for i in 13:8760)) == 0
 end
 
-# TODO test MPC with Generator
 # TODO test MPC with outages
 @testset "MPC" begin
     model = Model(optimizer_with_attributes(Cbc.Optimizer, "logLevel"=>0))
