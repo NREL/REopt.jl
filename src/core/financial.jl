5af7f56ab2cfdd58a1696ba1af8fe36ce34f4c35
Nick Laws <nick.laws@nrel.gov>
2021-07-31 17:23:19 -0600

    Financial: handle different tax/discount rates for non-third-party

--- src/core/financial.jl
+++ src/core/financial.jl
@@ -30,33 +30,79 @@
 """
     Financial
 
-    Base.@kwdef struct Financial
-        om_cost_escalation_pct::Float64 = 0.025
-        elec_cost_escalation_pct::Float64 = 0.023
-        offtaker_tax_pct::Float64 = 0.26
-        offtaker_discount_pct = 0.083
-        third_party_ownership::Bool = false
-        owner_tax_pct::Float64 = 0.26
-        owner_discount_pct::Float64 = 0.083
-        analysis_years::Int = 25
-        macrs_five_year::Array{Float64,1} = [0.2, 0.32, 0.192, 0.1152, 0.1152, 0.0576]  # IRS pub 946
-        macrs_seven_year::Array{Float64,1} = [0.1429, 0.2449, 0.1749, 0.1249, 0.0893, 0.0892, 0.0893, 0.0446]
-        VoLL::Union{Array{R,1}, R} where R<:Real = 1.00
-        microgrid_premium_pct::Float64 = 0.3
-    end
-
-"""
-Base.@kwdef struct Financial
-    om_cost_escalation_pct::Float64 = 0.025
-    elec_cost_escalation_pct::Float64 = 0.023
-    offtaker_tax_pct::Float64 = 0.26
-    offtaker_discount_pct = 0.083
-    third_party_ownership::Bool = false
-    owner_tax_pct::Float64 = 0.26
-    owner_discount_pct::Float64 = 0.083
-    analysis_years::Int = 25
-    macrs_five_year::Array{Float64,1} = [0.2, 0.32, 0.192, 0.1152, 0.1152, 0.0576]  # IRS pub 946
+Financial data struct with inner constructor:
+```julia
+function Financial(;
+    om_cost_escalation_pct::Float64 = 0.025,
+    elec_cost_escalation_pct::Float64 = 0.023,
+    offtaker_tax_pct::Float64 = 0.26,
+    offtaker_discount_pct = 0.083,
+    third_party_ownership::Bool = false,
+    owner_tax_pct::Float64 = 0.26,
+    owner_discount_pct::Float64 = 0.083,
+    analysis_years::Int = 25,
+    VoLL::Union{Array{R,1}, R} where R<:Real = 1.00,
+    microgrid_premium_pct::Float64 = 0.3,
+    macrs_five_year::Array{Float64,1} = [0.2, 0.32, 0.192, 0.1152, 0.1152, 0.0576],  # IRS pub 946
     macrs_seven_year::Array{Float64,1} = [0.1429, 0.2449, 0.1749, 0.1249, 0.0893, 0.0892, 0.0893, 0.0446]
-    VoLL::Union{Array{R,1}, R} where R<:Real = 1.00
-    microgrid_premium_pct::Float64 = 0.3
+)
+```
+
+!!! note
+    When `third_party_ownership` is `false` the offtaker's discount and tax percentages are used throughout the model:
+    ```julia
+        if !third_party_ownership
+            owner_tax_pct = offtaker_tax_pct
+            owner_discount_pct = offtaker_discount_pct
+        end
+    ```
+"""
+struct Financial
+    om_cost_escalation_pct::Float64
+    elec_cost_escalation_pct::Float64
+    offtaker_tax_pct::Float64
+    offtaker_discount_pct
+    third_party_ownership::Bool
+    owner_tax_pct::Float64
+    owner_discount_pct::Float64
+    analysis_years::Int
+    VoLL::Union{Array{R,1}, R} where R<:Real
+    microgrid_premium_pct::Float64
+    macrs_five_year::Array{Float64,1}
+    macrs_seven_year::Array{Float64,1}
+
+    function Financial(;
+        om_cost_escalation_pct::Float64 = 0.025,
+        elec_cost_escalation_pct::Float64 = 0.023,
+        offtaker_tax_pct::Float64 = 0.26,
+        offtaker_discount_pct = 0.083,
+        third_party_ownership::Bool = false,
+        owner_tax_pct::Float64 = 0.26,
+        owner_discount_pct::Float64 = 0.083,
+        analysis_years::Int = 25,
+        VoLL::Union{Array{R,1}, R} where R<:Real = 1.00,
+        microgrid_premium_pct::Float64 = 0.3,
+        macrs_five_year::Array{Float64,1} = [0.2, 0.32, 0.192, 0.1152, 0.1152, 0.0576],  # IRS pub 946
+        macrs_seven_year::Array{Float64,1} = [0.1429, 0.2449, 0.1749, 0.1249, 0.0893, 0.0892, 0.0893, 0.0446]
+    )
+        if !third_party_ownership
+            owner_tax_pct = offtaker_tax_pct
+            owner_discount_pct = offtaker_discount_pct
+        end
+
+        return new(
+            om_cost_escalation_pct,
+            elec_cost_escalation_pct,
+            offtaker_tax_pct,
+            offtaker_discount_pct,
+            third_party_ownership,
+            owner_tax_pct,
+            owner_discount_pct,
+            analysis_years,
+            VoLL,
+            microgrid_premium_pct,
+            macrs_five_year,
+            macrs_seven_year
+        )
+    end
 end
