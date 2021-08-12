3667098b261902e082055b84a782af4032c0257b
Nick Laws <nick.laws@nrel.gov>
2021-08-12 13:15:57 -0600

    another attempt at making SSC work on Windows
    also moved the wind test up to make the test suite fail fast

--- src/core/prodfactor.jl
+++ src/core/prodfactor.jl
@@ -185,7 +185,9 @@ function prodfactor(wind::Wind, latitude::Real, longitude::Real, time_steps_per_
         if Sys.islinux() || Sys.isapple()
             hdl = Libc.Libdl.dlopen(joinpath(dirname(@__FILE__), "..", "sam", libfile), Libc.Libdl.RTLD_GLOBAL)
         else
-            hdl = Libc.Libdl.dlopen(joinpath(dirname(@__FILE__), "..", "sam", libfile))
+            hdl = Libc.Libdl.dlopen(joinpath(dirname(@__FILE__), "..", "sam", libfile),
+                Libc.Libdl.RTLD_LAZY|Libc.Libdl.RTLD_DEEPBIND|Libc.Libdl.RTLD_GLOBAL
+            )
         end
         wind_module = @ccall ssc_module_create("windpower"::Cstring)::Ptr{Cvoid}
         wind_resource = @ccall ssc_data_create()::Ptr{Cvoid}  # data pointer
