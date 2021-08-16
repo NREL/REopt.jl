347d95bf1a6b44ad84fb49b354738effb711f5d1
Nick Laws <nick.laws@nrel.gov>
2021-08-16 17:14:38 -0600

    test wind on Windows (Cbc solver)

--- src/core/prodfactor.jl
+++ src/core/prodfactor.jl
@@ -27,6 +27,7 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 # *********************************************************************************
+
 function prodfactor(pv::PV, latitude::Real, longitude::Real; timeframe="hourly")
 
     if !(ismissing(pv.prod_factor_series_kw))
@@ -136,7 +137,7 @@ function prodfactor(wind::Wind, latitude::Real, longitude::Real, time_steps_per_
     end  # done filling in resources (can contain one Vector of Vectors or multiple Vectors of Vectors)
 
     # Initialize SAM inputs
-    hdl = nothing
+    global hdl = nothing
     sam_prodfactor = []
 
     # Corresponding size in kW for generic reference turbines sizes
@@ -175,38 +176,53 @@ function prodfactor(wind::Wind, latitude::Real, longitude::Real, time_steps_per_
             libfile = "libssc.dylib"
         elseif Sys.islinux()
             libfile = "libssc.so"
-        # elseif Sys.iswindows()
-        #     libfile = "ssc.dll"
+        elseif Sys.iswindows()
+            libfile = "ssc.dll"
         # TODO: Cannot get the ssc.dll to export the C++ functions 
         # https://discourse.julialang.org/t/ccall-on-windows-could-not-load-symbol-the-specified-procedure-could-not-be-found/66291
+        """
+    
+    hdl = Libc.Libdl.dlopen("ssc.dll")
+    ssc_module_create = Libc.Libdl.dlsym(hdl, :ssc_module_create)
+    wind_module = @ccall $ssc_module_create("windpower"::Cstring)::Ptr{Cvoid}
+
+    ssc_data_create = Libc.Libdl.dlsym(hdl, :ssc_data_create)
+    wind_resource = @ccall $ssc_data_create()::Ptr{Cvoid}
+
+    ssc_data_set_number = Libc.Libdl.dlsym(hdl, :ssc_data_set_number)
+    @ccall $ssc_data_set_number(wind_resource::Ptr{Cvoid}, "latitude"::Cstring, 45.3::Cdouble)::Cvoid
+    
+    Libc.Libd.dlclose(hdl)  # in case you already have a handle open
+
+        """
         else
             @error """Unsupported platform for using the SAM Wind module. 
                       You can alternatively provide the Wind.prod_factor_series_kw"""
         end
         
-        hdl = Libc.Libdl.dlopen(joinpath(dirname(@__FILE__), "..", "sam", libfile), Libc.Libdl.RTLD_GLOBAL)
-        
-        wind_module = @ccall ssc_module_create("windpower"::Cstring)::Ptr{Cvoid}
-        wind_resource = @ccall ssc_data_create()::Ptr{Cvoid}  # data pointer
-        @ccall ssc_module_exec_set_print(0::Cint)::Cvoid
+        # hdl = Libc.Libdl.dlopen(joinpath(dirname(@__FILE__), "..", "sam", libfile), Libc.Libdl.RTLD_GLOBAL)
+        global hdl = joinpath(dirname(@__FILE__), "..", "sam", libfile)
+        wind_module = @ccall hdl.ssc_module_create("windpower"::Cstring)::Ptr{Cvoid}
+        wind_resource = @ccall hdl.ssc_data_create()::Ptr{Cvoid}  # data pointer
+        @ccall hdl.ssc_module_exec_set_print(0::Cint)::Cvoid
 
-        @ccall ssc_data_set_number(wind_resource::Ptr{Cvoid}, "latitude"::Cstring, latitude::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(wind_resource::Ptr{Cvoid}, "longitude"::Cstring, longitude::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(wind_resource::Ptr{Cvoid}, "elevation"::Cstring, 0::Cdouble)::Cvoid  # not used in SAM
-        @ccall ssc_data_set_number(wind_resource::Ptr{Cvoid}, "year"::Cstring, 2012::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(wind_resource::Ptr{Cvoid}, "latitude"::Cstring, latitude::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(wind_resource::Ptr{Cvoid}, "longitude"::Cstring, longitude::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(wind_resource::Ptr{Cvoid}, "elevation"::Cstring, 0::Cdouble)::Cvoid  # not used in SAM
+        @ccall hdl.ssc_data_set_number(wind_resource::Ptr{Cvoid}, "year"::Cstring, 2012::Cdouble)::Cvoid
 
         heights_array = []  # have to repeat heights for each resource column
         for h in heights_for_sam
             append!(heights_array, repeat([h], 4))
         end
         heights_array = convert(Array{Float64}, heights_array)
-        @ccall ssc_data_set_array(wind_resource::Ptr{Cvoid}, "heights"::Cstring, 
+        @ccall hdl.ssc_data_set_array(wind_resource::Ptr{Cvoid}, "heights"::Cstring, 
            heights_array::Ptr{Cdouble}, length(heights_array)::Cint)::Cvoid
 
         # setup column data types: temperature=1, pressure=2, degree=3, speed=4
         fields = collect(repeat(range(1, stop=4), length(heights_for_sam)))
         fields = convert(Array{Float64}, fields)
-        @ccall ssc_data_set_array(wind_resource::Ptr{Cvoid}, "fields"::Cstring, 
+        @ccall hdl.ssc_data_set_array(wind_resource::Ptr{Cvoid}, "fields"::Cstring, 
             fields::Ptr{Cdouble}, length(fields)::Cint)::Cvoid
 
         nrows, ncols = size(resources)
@@ -214,66 +230,66 @@ function prodfactor(wind::Wind, latitude::Real, longitude::Real, time_steps_per_
         t2 = reduce(vcat, t);
         # the values in python api are sent to SAM as vector (35040) with rows concatenated
         c_resources = [convert(Float64, t2[i]) for i in eachindex(t2)]
-        @ccall ssc_data_set_matrix(wind_resource::Ptr{Cvoid}, "data"::Cstring, c_resources::Ptr{Cdouble}, 
+        @ccall hdl.ssc_data_set_matrix(wind_resource::Ptr{Cvoid}, "data"::Cstring, c_resources::Ptr{Cdouble}, 
             Cint(nrows)::Cint, Cint(ncols)::Cint)::Cvoid
 
-        data = @ccall ssc_data_create()::Ptr{Cvoid}  # data pointer
-        @ccall ssc_data_set_table(data::Ptr{Cvoid}, "wind_resource_data"::Cstring, wind_resource::Ptr{Cvoid})::Cvoid
-        @ccall ssc_data_free(wind_resource::Ptr{Cvoid})::Cvoid
+        data = @ccall hdl.ssc_data_create()::Ptr{Cvoid}  # data pointer
+        @ccall hdl.ssc_data_set_table(data::Ptr{Cvoid}, "wind_resource_data"::Cstring, wind_resource::Ptr{Cvoid})::Cvoid
+        @ccall hdl.ssc_data_free(wind_resource::Ptr{Cvoid})::Cvoid
 
         # # can get the same values back with:
-        # a = @ccall ssc_data_get_matrix(wind_resource::Ptr{Cvoid}, "data"::Cstring, Ref(Cint(4))::Ptr{Cint}, Ref(Cint(4))::Ptr{Cint})::Ptr{Cdouble}
+        # a = @ccall hdl.ssc_data_get_matrix(wind_resource::Ptr{Cvoid}, "data"::Cstring, Ref(Cint(4))::Ptr{Cint}, Ref(Cint(4))::Ptr{Cint})::Ptr{Cdouble}
         # unsafe_load(a, 1)
         # unsafe_load(a, 35040)
 
         # Scaler inputs
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_resource_shear"::Cstring, 0.14000000059604645::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_resource_turbulence_coeff"::Cstring, 
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_resource_shear"::Cstring, 0.14000000059604645::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_resource_turbulence_coeff"::Cstring, 
             0.10000000149011612::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "system_capacity"::Cstring, 
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "system_capacity"::Cstring, 
             system_capacity::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_resource_model_choice"::Cstring, 0::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "weibull_reference_height"::Cstring, 50::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "weibull_k_factor"::Cstring, 2::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "weibull_wind_speed"::Cstring, 7.25::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_turbine_rotor_diameter"::Cstring, 
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_resource_model_choice"::Cstring, 0::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "weibull_reference_height"::Cstring, 50::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "weibull_k_factor"::Cstring, 2::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "weibull_wind_speed"::Cstring, 7.25::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_turbine_rotor_diameter"::Cstring, 
             rotor_diameter_lookup[wind.size_class]::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_turbine_hub_ht"::Cstring, wind.hub_height::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_turbine_max_cp"::Cstring, 0.44999998807907104::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_farm_losses_percent"::Cstring, 0::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "wind_farm_wake_model"::Cstring, 0::Cdouble)::Cvoid
-        @ccall ssc_data_set_number(data::Ptr{Cvoid}, "adjust:constant"::Cstring, 0::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_turbine_hub_ht"::Cstring, wind.hub_height::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_turbine_max_cp"::Cstring, 0.44999998807907104::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_farm_losses_percent"::Cstring, 0::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "wind_farm_wake_model"::Cstring, 0::Cdouble)::Cvoid
+        @ccall hdl.ssc_data_set_number(data::Ptr{Cvoid}, "adjust:constant"::Cstring, 0::Cdouble)::Cvoid
 
         # Array inputs
         speeds = convert(Array{Float64},
             [0., 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
-        @ccall ssc_data_set_array(data::Ptr{Cvoid}, "wind_turbine_powercurve_windspeeds"::Cstring, 
+        @ccall hdl.ssc_data_set_array(data::Ptr{Cvoid}, "wind_turbine_powercurve_windspeeds"::Cstring, 
             speeds::Ptr{Cdouble}, length(speeds)::Cint)::Cvoid
         
         powercurve = convert(Array{Float64}, wind_turbine_powercurve_lookup[wind.size_class])
-        @ccall ssc_data_set_array(data::Ptr{Cvoid}, "wind_turbine_powercurve_powerout"::Cstring, 
+        @ccall hdl.ssc_data_set_array(data::Ptr{Cvoid}, "wind_turbine_powercurve_powerout"::Cstring, 
             powercurve::Ptr{Cdouble}, length(powercurve)::Cint)::Cvoid
         
         wind_farm_xCoordinates = [Float64(0)]
-        @ccall ssc_data_set_array(data::Ptr{Cvoid}, "wind_farm_xCoordinates"::Cstring, 
+        @ccall hdl.ssc_data_set_array(data::Ptr{Cvoid}, "wind_farm_xCoordinates"::Cstring, 
             wind_farm_xCoordinates::Ptr{Cdouble}, 1::Cint)::Cvoid
         
         wind_farm_yCoordinates = [Float64(0)]
-        @ccall ssc_data_set_array(data::Ptr{Cvoid}, "wind_farm_yCoordinates"::Cstring, 
+        @ccall hdl.ssc_data_set_array(data::Ptr{Cvoid}, "wind_farm_yCoordinates"::Cstring, 
             wind_farm_yCoordinates::Ptr{Cdouble}, 1::Cint)::Cvoid
 
         # example of getting a number:
         # val = convert(Cdouble, 0.0)
         # ref = Ref(val)
-        # @ccall ssc_data_get_number(data::Ptr{Cvoid}, "wind_resource_shear"::Cstring, ref::Ptr{Cdouble})::Cvoid
+        # @ccall hdl.ssc_data_get_number(data::Ptr{Cvoid}, "wind_resource_shear"::Cstring, ref::Ptr{Cdouble})::Cvoid
         # Float64(ref[])
         
-        if !Bool(@ccall ssc_module_exec(wind_module::Ptr{Cvoid}, data::Ptr{Cvoid})::Cint)
+        if !Bool(@ccall hdl.ssc_module_exec(wind_module::Ptr{Cvoid}, data::Ptr{Cvoid})::Cint)
             log_type = 0
             log_type_ref = Ref(log_type)
             log_time = 0
             log_time_ref = Ref(log_time)
-            msg_ptr = @ccall ssc_module_log(wind_module::Ptr{Cvoid}, 0::Cint, log_type_ref::Ptr{Cvoid}, 
+            msg_ptr = @ccall hdl.ssc_module_log(wind_module::Ptr{Cvoid}, 0::Cint, log_type_ref::Ptr{Cvoid}, 
                                             log_time_ref::Ptr{Cvoid})::Cstring
             msg = "no message from ssc_module_log."
             try
@@ -285,15 +301,15 @@ function prodfactor(wind::Wind, latitude::Real, longitude::Real, time_steps_per_
 
         len = 0
         len_ref = Ref(len)
-        a = @ccall ssc_data_get_array(data::Ptr{Cvoid}, "gen"::Cstring, len_ref::Ptr{Cvoid})::Ptr{Float64}
+        a = @ccall hdl.ssc_data_get_array(data::Ptr{Cvoid}, "gen"::Cstring, len_ref::Ptr{Cvoid})::Ptr{Float64}
         for i in range(1, stop=8760)
             push!(sam_prodfactor, unsafe_load(a, i))
         end
-        @ccall ssc_module_free(wind_module::Ptr{Cvoid})::Cvoid   
-        @ccall ssc_data_free(data::Ptr{Cvoid})::Cvoid
+        @ccall hdl.ssc_module_free(wind_module::Ptr{Cvoid})::Cvoid   
+        @ccall hdl.ssc_data_free(data::Ptr{Cvoid})::Cvoid
 
     finally
-        Libc.Libdl.dlclose(hdl)
+        # Libc.Libdl.dlclose(hdl)
     end
 
     if !(length(sam_prodfactor) == 8760)
