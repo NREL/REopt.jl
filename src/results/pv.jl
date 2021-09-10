00145fe6a3ff20e904b5cae1f35c2939418b6d9e
Nick Laws <nick.laws@nrel.gov>
2021-09-10 15:06:21 -0600

    add PV.average_yearly_energy_produced_kwh output

--- src/results/pv.jl
+++ src/results/pv.jl
@@ -60,6 +60,7 @@ function add_pv_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
 		r["year_one_to_load_series_kw"] = round.(value.(PVtoLoad), digits=3)
 		Year1PvProd = (sum(m[Symbol("dvRatedProduction"*_n)][t,ts] * p.production_factor[t, ts] for ts in p.time_steps) * p.hours_per_timestep)
 		r["year_one_energy_produced_kwh"] = round(value(Year1PvProd), digits=0)
+        r["average_yearly_energy_produced_kwh"] = r["year_one_energy_produced_kwh"] * p.levelization_factor[t]
 		PVPerUnitSizeOMCosts = p.om_cost_per_kw[t] * p.pwf_om * m[Symbol("dvSize"*_n)][t]
 		r["total_om_cost"] = round(value(PVPerUnitSizeOMCosts) * (1 - p.s.financial.owner_tax_pct), digits=0)
         d[t] = r
