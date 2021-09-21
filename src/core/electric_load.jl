3807c3154a044250595cf49befa3e2c95d86f408
Nick Laws <nick.laws@nrel.gov>
2021-09-21 14:01:58 -0600

    create doe_commercial_reference_building_loads.jl
    base support functions for elec or thermal CRB loads

--- src/core/electric_load.jl
+++ src/core/electric_load.jl
@@ -128,8 +128,7 @@ mutable struct ElectricLoad  # mutable to adjust (critical_)loads_kw based off o
                 @warn "Changing ElectricLoad.year to 2017 because DOE reference profiles start on a Sunday."
             end
             year = 2017
-            loads_kw = BuiltInElectricLoad(city, doe_reference_name, latitude, longitude, year, annual_kwh=annual_kwh, 
-                                           monthly_totals_kwh=monthly_totals_kwh)
+            loads_kw = BuiltInElectricLoad(city, doe_reference_name, latitude, longitude, year, annual_kwh, monthly_totals_kwh)
             if ismissing(critical_loads_kw)
                 critical_loads_kw = critical_load_pct * loads_kw
             end
@@ -171,12 +170,11 @@ function BuiltInElectricLoad(
     buildingtype::String,
     latitude::Float64,
     longitude::Float64,
-    year::Int;
+    year::Int,
     annual_kwh::Union{<:Real, Nothing}=nothing,
     monthly_totals_kwh::Union{<:Real, Vector{<:Real}}=nothing,
     )
-    monthly_scalers = ones(12)
-    lib_path = joinpath(dirname(@__FILE__), "..", "..", "data", "load_profiles", "electric")
+    
     annual_loads = Dict(
         "Albuquerque" => Dict(
             "fastfoodrest" => 193235,
@@ -464,25 +462,6 @@ function BuiltInElectricLoad(
             "flatload" => 500000
         ),
     )
-    default_buildings = [
-        "FastFoodRest",
-        "FullServiceRest",
-        "Hospital",
-        "LargeHotel",
-        "LargeOffice",
-        "MediumOffice",
-        "MidriseApartment",
-        "Outpatient",
-        "PrimarySchool",
-        "RetailStore",
-        "SecondarySchool",
-        "SmallHotel",
-        "SmallOffice",
-        "StripMall",
-        "Supermarket",
-        "Warehouse",
-        "FlatLoad",
-    ]
     if !(buildingtype in default_buildings)
         error("buildingtype $(buildingtype) not in $(default_buildings).")
     end
@@ -491,137 +470,9 @@ function BuiltInElectricLoad(
         city = find_ashrae_zone_city(latitude, longitude)
     end
 
-    profile_path = joinpath(lib_path, string("Load8760_norm_" * city * "_" * buildingtype * ".dat"))
-    normalized_profile = vec(readdlm(profile_path, '\n', Float64, '\n'))
-    
-    if length(monthly_totals_kwh) == 12
-        annual_kwh = 1.0
-        t0 = 1
-        for month in 1:12
-            plus_hours = daysinmonth(Date(string(year) * "-" * string(month))) * 24
-            if month == 2 && isleapyear(year)
-                plus_hours -= 24
-            end
-            month_total = sum(normalized_profile[t0:t0+plus_hours-1])
-            if month_total == 0.0  # avoid division by zero
-                monthly_scalers[month] = 0.0
-            else
-                monthly_scalers[month] = monthly_totals_kwh[month] / month_total
-            end
-            t0 += plus_hours
-        end
-    elseif isnothing(annual_kwh)
+    if isnothing(annual_kwh)
         annual_kwh = annual_loads[city][lowercase(buildingtype)]
     end
 
-    scaled_load = Float64[]
-    datetime = DateTime(year, 1, 1, 1)
-    for ld in normalized_profile
-        month = Month(datetime).value
-        push!(scaled_load, ld * annual_kwh * monthly_scalers[month])
-        datetime += Dates.Hour(1)
-    end
-
-    return scaled_load
-end
-
-
-function find_ashrae_zone_city(lat, lon)::String
-    file_path = joinpath(dirname(@__FILE__), "..", "..", "data", "climate_cities.shp")
-    table = Shapefile.Table(file_path)
-    geoms = Shapefile.shapes(table)
-    # TODO following for loop is relatively slow
-    for (row, geo) in enumerate(geoms)
-        g = length(geo.points)
-        nodes = zeros(g, 2)
-        edges = zeros(g, 2)
-        for (i,p) in enumerate(geo.points)
-            nodes[i,:] = [p.x, p.y]
-            edges[i,:] = [i, i+1]
-        end
-        edges[g, :] = [g, 1]
-        edges = convert(Array{Int64,2}, edges)
-        # shapefiles have longitude as x, latitude as y  
-        if inpoly2([lon, lat], nodes, edges)[1]
-            return table.city[row]
-        end
-        GC.gc()
-    end
-    @info "Could not find latitude/longitude in U.S. Using geometrically nearest city."
-    cities = [
-        (city="Miami", lat=25.761680, lon=-80.191790),
-        (city="Houston", lat=29.760427, lon=-95.369803),
-        (city="Phoenix", lat=33.448377, lon=-112.074037),
-        (city="Atlanta", lat=33.748995, lon=-84.387982),
-        (city="LasVegas", lat=36.1699, lon=-115.1398),
-        (city="LosAngeles", lat=34.052234, lon=-118.243685),
-        (city="SanFrancisco", lat=37.3382, lon=-121.8863),
-        (city="Baltimore", lat=39.290385, lon=-76.612189),
-        (city="Albuquerque", lat=35.085334, lon=-106.605553),
-        (city="Seattle", lat=47.606209, lon=-122.332071),
-        (city="Chicago", lat=41.878114, lon=-87.629798),
-        (city="Boulder", lat=40.014986, lon=-105.270546),
-        (city="Minneapolis", lat=44.977753, lon=-93.265011),
-        (city="Helena", lat=46.588371, lon=-112.024505,),
-        (city="Duluth", lat=46.786672, lon=-92.100485),
-        (city="Fairbanks", lat=59.0397, lon=-158.4575),
-    ]
-    min_distance = 0.0
-    nearest_city = ""
-    for (i, c) in enumerate(cities)
-        distance = sqrt((lat - c.lat)^2 + (lon - c.lon)^2)
-        if i == 1
-            min_distance = distance
-            nearest_city = c.city
-        elseif distance < min_distance
-            min_distance = distance
-            nearest_city = c.city
-        end
-    end
-    return nearest_city
-end
-
-
-function blend_and_scale_doe_profiles(
-    constructor,
-    latitude::Float64,
-    longitude::Float64,
-    year::Int = 2017,
-    blended_doe_reference_names::Array{String, 1} = String[],
-    blended_doe_reference_percents::Array{<:Real,1} = Real[],
-    city::String = "",
-    annual_kwh::Union{Real, Nothing} = nothing,
-    monthly_totals_kwh::Array{<:Real,1} = Real[],
-    )
-
-    @assert sum(blended_doe_reference_percents) ≈ 1 "The sum of the blended_doe_reference_percents must equal 1"
-    if year != 2017
-        @warn "Changing ElectricLoad.year to 2017 because DOE reference profiles start on a Sunday."
-    end
-    year = 2017
-    if isempty(city)
-        city = find_ashrae_zone_city(latitude, longitude)  # avoid redundant look-ups
-    end
-    profiles = Array[]  # collect the built in profiles
-    for name in blended_doe_reference_names
-        push!(profiles, constructor(city, name, latitude, longitude, year, annual_kwh=annual_kwh, 
-                                    monthly_totals_kwh=monthly_totals_kwh))
-    end
-    if isnothing(annual_kwh) # then annual_kwh should be the sum of all the profiles' annual kwhs
-        # we have to rescale the built in profiles to the total_kwh by normalizing them with their
-        # own annual kwh and multiplying by the total kwh
-        annual_kwhs = [sum(profile) for profile in profiles]
-        total_kwh = sum(annual_kwhs)
-        monthly_scaler = 1
-        if length(monthly_totals_kwh) == 12
-            monthly_scaler = length(blended_doe_reference_names)
-        end
-        for idx in 1:length(profiles)
-            profiles[idx] .*= total_kwh / annual_kwhs[idx] / monthly_scaler
-        end
-    end
-    for idx in 1:length(profiles)  # scale the profiles
-        profiles[idx] .*= blended_doe_reference_percents[idx]
-    end
-    sum(profiles)
+    built_in_load("electric", city, buildingtype, year, annual_kwh, monthly_totals_kwh)
 end
