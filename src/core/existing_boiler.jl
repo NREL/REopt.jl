53753b54bb48802c0beea7ad32c2111d997c8291
Nick Laws <nick.laws@nrel.gov>
2021-09-23 07:15:17 -0600

    update logic for defining Scenario thermal loads
    if either SpaceHeatingLoad or DomesticHotWaterLoad are provide then create ExistingBoiler

--- src/core/existing_boiler.jl
+++ src/core/existing_boiler.jl
@@ -30,43 +30,52 @@
 struct ExistingBoiler <: AbstractThermalTech  # useful to create AbstractHeatingTech or AbstractThermalTech?
     max_kw::Real
     efficiency::Real
+    fuel_cost_series::AbstractVector{<:Real}
+end
 
-    function ExistingBoiler(;
-        production_type::String = "hot_water",
-        chp_prime_mover::String = "",
-        # max_thermal_factor_on_peak_load::Real,
-        # production_type::String,
-        efficiency::Real = 0.0,
-        # can_supply_steam_turbine::Bool,
-        # emissions_factor_lb_CO2_per_mmbtu::Real,
-        max_kw::Real=0
+
+function ExistingBoiler(;
+    max_heat_demand_kw::Real=0,
+    production_type::String = "hot_water",
+    chp_prime_mover::String = "",
+    max_thermal_factor_on_peak_load::Real = 1.25,
+    efficiency::Real = 0.0,
+    fuel_cost_per_mmbtu::Union{<:Real, AbstractVector{<:Real}} = 0.0,
+    time_steps_per_hour::Int = 1
+    # fuel_type::String = "natural_gas"  # "restrict_to": ["natural_gas", "landfill_bio_gas", "propane", "diesel_oil"],
+    # can_supply_steam_turbine::Bool,
+    # emissions_factor_lb_CO2_per_mmbtu::Real,
+)
+    @assert production_type in ["steam", "hot_water"]
+
+    production_type_by_chp_prime_mover = Dict(
+        "recip_engine" => "hot_water",
+        "micro_turbine" => "hot_water",
+        "combustion_turbine" => "steam",
+        "fuel_cell" => "hot_water"
     )
-        @assert production_type in ["steam", "hot_water"]
 
-        production_type_by_chp_prime_mover = Dict(
-            "recip_engine" => "hot_water",
-            "micro_turbine" => "hot_water",
-            "combustion_turbine" => "steam",
-            "fuel_cell" => "hot_water"
-        )
+    fuel_cost_per_kwh = fuel_cost_per_mmbtu / mmbtu_to_kwh
+    fuel_cost_series = per_hour_value_to_time_series(fuel_cost_per_kwh, time_steps_per_hour, 
+                                                     "ExistingBoiler.fuel_cost_per_mmbtu")
 
-        efficiency_defaults = Dict(
-            "hot_water" => 0.80,
-            "steam" => 0.75
-        )
+    efficiency_defaults = Dict(
+        "hot_water" => 0.80,
+        "steam" => 0.75
+    )
 
-        if efficiency == 0.0 && max_kw > 0
-            if !isempty(chp_prime_mover)
-                production_type = production_type_by_chp_prime_mover[chp_prime_mover]
-            end
-            efficiency = efficiency_defaults[production_type]
+    if efficiency == 0.0
+        if !isempty(chp_prime_mover)
+            production_type = production_type_by_chp_prime_mover[chp_prime_mover]
         end
-
-        # max_kw = max(boiler_fuel_series_bau) * self.boiler_efficiency * self.max_thermal_factor_on_peak_load * mmbtu_to_kwh
-
-        new(
-            max_kw,
-            efficiency
-        )
+        efficiency = efficiency_defaults[production_type]
     end
+
+    max_kw = max_heat_demand_kw * max_thermal_factor_on_peak_load
+
+    ExistingBoiler(
+        max_kw,
+        efficiency,
+        fuel_cost_series
+    )
 end
