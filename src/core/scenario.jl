6e1d4029b9ffbaf96f7f068c9c818ee9f5e78e4d
zolanaj <alexander.zolan@nrel.gov>
2022-02-07 21:02:55 -0700

    update storage handling in scenario.jl

--- src/core/scenario.jl
+++ src/core/scenario.jl
@@ -32,7 +32,9 @@ struct Scenario <: AbstractScenario
     site::Site
     pvs::Array{PV, 1}
     wind::Wind
-    storage::Storage
+    elec_storage::ElectricStorage
+    hot_tes::HotThermalStorage
+    cold_tes::ColdThermalStorage
     electric_tariff::ElectricTariff
     electric_load::ElectricLoad
     electric_utility::ElectricUtility
@@ -129,31 +131,31 @@ function Scenario(d::Dict)
         electric_utility = ElectricUtility()
     end
 
-    if haskey(d, "Storage")
+    if haskey(d, "ElectricStorage")
         # only modeling electrochemical storage so far
-        storage_dict = Dict(:elec => dictkeys_tosymbols(d["Storage"]))
-        storage = Storage(storage_dict, financial)
+        storage_dict = Dict(:elec => dictkeys_tosymbols(d["ElectricStorage"]))
+        elec_storage = Storage(storage_dict, financial)
     else
         storage_dict = Dict(:elec => Dict(:max_kw => 0))
-        storage = Storage(storage_dict, financial)
+        elec_storage = Storage(storage_dict, financial)
     end
 
-    if haskey(d, "HotTES")
+    if haskey(d, "HotThermalStorage")
         # only modeling electrochemical storage so far
-        hot_storage_dict = Dict(:elec => dictkeys_tosymbols(d["HotTES"]))
-        hot_storage = Storage(storage_dict, financial)
+        hot_storage_dict = Dict(:hot_tes => dictkeys_tosymbols(d["HotThermalStorage"]))
+        hot_tes = Storage(hot_storage_dict, financial)
     else
-        hot_storage_dict = Dict(:elec => Dict(:max_kwh => 0))
-        hot_storage = Storage(storage_dict, financial)
+        hot_storage_dict = Dict(:hot_tes => Dict(:max_kwh => 0))
+        hot_tes = Storage(hot_storage_dict, financial)
     end
 
-    if haskey(d, "ColdTES")
+    if haskey(d, "ColdThermalStorage")
         # only modeling electrochemical storage so far
-        cold_storage_dict = Dict(:elec => dictkeys_tosymbols(d["ColdTES"]))
-        cold_storage = Storage(storage_dict, financial)
+        cold_storage_dict = Dict(:cold_tes => dictkeys_tosymbols(d["ColdThermalStorage"]))
+        cold_tes = Storage(cold_storage_dict, financial)
     else
-        cold_storage_dict = Dict(:elec => Dict(:max_kwh => 0))
-        cold_storage = Storage(storage_dict, financial)
+        cold_storage_dict = Dict(:cold_tes => Dict(:max_kwh => 0))
+        cold_tes = Storage(cold_storage_dict, financial)
     end
 
 
@@ -306,7 +308,9 @@ function Scenario(d::Dict)
         site, 
         pvs, 
         wind,
-        storage, 
+        elec_storage,
+        hot_tes,
+        cold_tes, 
         electric_tariff, 
         electric_load, 
         electric_utility, 
