f9a94d4c3b4ec4d819816c66a35f33510f61095a
adfarth <adfarth@umich.edu>
2022-03-29 12:42:26 -0600

    only apply generator min turndown for off_grid_flag and time_steps_with_grid

--- src/core/generator.jl
+++ src/core/generator.jl
@@ -118,7 +118,7 @@ struct Generator <: AbstractGenerator
         fuel_intercept_gal_per_hr::Float64 = 0.0,
         fuel_avail_gal::Float64 = off_grid_flag ? 1.0e9 : 660.0,
         min_turn_down_pct::Float64 = off_grid_flag ? 0.15 : 0.0,
-        only_runs_during_grid_outage::Bool = true, ## TODO check if this needs to be false in off-grid?
+        only_runs_during_grid_outage::Bool = true,
         sells_energy_back_to_grid::Bool = false,
         can_net_meter::Bool = false,
         can_wholesale::Bool = false,
