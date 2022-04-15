df96990857e34b56f4d28d37d0fe7de19f945118
adfarth <adfarth@umich.edu>
2022-04-15 14:55:46 -0600

    handle type conversions

--- src/core/utils.jl
+++ src/core/utils.jl
@@ -203,7 +203,10 @@ function dictkeys_tosymbols(d::Dict)
         end
         if k in [
             "installed_cost_per_kw", "replace_cost_per_kw",
-            "offgrid_other_capital_costs", "offgrid_other_annual_costs"
+            "offgrid_other_capital_costs", "offgrid_other_annual_costs",
+            "land_acres", "roof_squarefeet", "federal_rebate_per_kw",
+            "offtaker_tax_pct", "owner_tax_pct", "offtaker_discount_pct",
+            "owner_discount_pct", "om_cost_escalation_pct"
         ]
             try
                 v = convert(Float64, v)
