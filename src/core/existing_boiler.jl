613e67e80fd3cb09c6e29624feee31af6584b588
hdunham <70401017+hdunham@users.noreply.github.com>
2022-05-13 09:15:42 -0600

    make ExistingBoiler struct mutable
    in order to set missing emissions factors based on fuel type

--- src/core/existing_boiler.jl
+++ src/core/existing_boiler.jl
@@ -28,7 +28,7 @@
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 # *********************************************************************************
 
-struct ExistingBoiler <: AbstractThermalTech  # useful to create AbstractHeatingTech or AbstractThermalTech?
+mutable struct ExistingBoiler <: AbstractThermalTech  # useful to create AbstractHeatingTech or AbstractThermalTech?
     max_kw::Real
     efficiency::Real
     fuel_cost_series::AbstractVector{<:Real}
