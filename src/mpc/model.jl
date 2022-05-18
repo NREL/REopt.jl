d1c807a04ad904185b74de68207603be9ddbb44a
adfarth <adfarth@umich.edu>
2022-05-18 15:05:03 -0600

    remove TotalPerUnitHourOMCosts from mpc

--- src/mpc/model.jl
+++ src/mpc/model.jl
@@ -101,7 +101,7 @@ end
     build_mpc!(m::JuMP.AbstractModel, p::MPCInputs)
 
 Add variables and constraints for model predictive control model. 
-Similar to a REopt model but with any length of horizon (instead of one calendar year,
+Similar to a REopt model but with any length of horizon (instead of one calendar year),
 and the DER sizes must be provided.
 """
 function build_mpc!(m::JuMP.AbstractModel, p::MPCInputs)
