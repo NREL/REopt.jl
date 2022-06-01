a93826654de3639bdb09c731a44868631f1a4a1a
Nick Laws <nick.laws@nrel.gov>
2022-06-01 11:07:26 -0600

    Update reopt.jl

--- src/core/reopt.jl
+++ src/core/reopt.jl
@@ -247,9 +247,9 @@ function build_reopt!(m::JuMP.AbstractModel, p::REoptInputs)
             add_cooling_tech_constraints(m, p)
         end
     
-		m[Symbol("binFlexHVAC"*_n)] = 0
-		m[Symbol("dvTemperature"*_n)] = 0
-		m[Symbol("dvComfortLimitViolationCost"*_n)] = 0
+		m[Symbol("binFlexHVAC")] = 0
+		m[Symbol("dvTemperature")] = 0
+		m[Symbol("dvComfortLimitViolationCost")] = 0
 
         if !isempty(p.techs.thermal)
             add_thermal_load_constraints(m, p)  # split into heating and cooling constraints?
