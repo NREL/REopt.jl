7e0ce76eebe5c61483f38c9f0fe55b1ed9821c1c
hdunham <70401017+hdunham@users.noreply.github.com>
2022-06-13 12:48:26 -0600

    refine thermosyphon inputs

--- src/core/thermosyphon.jl
+++ src/core/thermosyphon.jl
@@ -36,10 +36,14 @@ function Thermosyphon(;
     ground_temp_degF::Real=25,
     passive_to_active_cutoff_temp_degF::Real=20,
     effective_conductance_btu_per_degF::Real=141,
-    design_active_cooling_rate_btu_per_hour::Real=0.197, #TODO: change # to from kw to btu/hour
+    fixed_active_cooling_rate_kw::Real=0.345, # average cooling rate from most recent data, was 0.197 kw
     COP_curve_points_ambient_temp_degF::Array{<:Real,1} = Real[], #ordered from lowest to highest temperature
-    COP_curve_points_coefficient_of_performance_kw_per_kw::Array{<:Real,1} = Real[], #one-to-one with COP_curve_points_ambient_temp_degF
-    structure_heat_to_ground_mmbtu_per_year::Real=5.9
+    COP_curve_points_COP_kw_per_kw::Union{Real,Array{<:Real,1}} = Real[], #one-to-one with COP_curve_points_ambient_temp_degF
+    structure_heat_to_ground_mmbtu_per_year::Real=5.9,
+    latitude::Float64,
+    longitude::Float64
+
+    design_active_cooling_rate_btu_per_hour::Real=0.197, #TODO: change # to from kw to btu/hour
 )
 ```
 """
@@ -50,9 +54,6 @@ struct Thermosyphon <: AbstractTech
     active_cooling_rate_mmbtu_per_hour
     time_steps_can_actively_cool
     time_steps_passively_cooling
-    # effective_conductance_btu_per_degF
-    # design_active_cooling_rate_kw
-    # structure_heat_to_ground_mmbtu_per_year
 
     function Thermosyphon(;
         ambient_temp_degF::Array{<:Real,1} = Real[],
@@ -60,8 +61,8 @@ struct Thermosyphon <: AbstractTech
         passive_to_active_cutoff_temp_degF::Real=20,
         effective_conductance_btu_per_degF::Real=141,
         fixed_active_cooling_rate_kw::Real=0.345, # average cooling rate from most recent data, was 0.197 kw
-        COP_curve_points_ambient_temp_degF::Array{<:Real,1} = Real[],
-        COP_curve_points_coefficient_of_performance_kw_per_kw::Array{<:Real,1} = Real[],
+        COP_curve_points_ambient_temp_degF::Array{<:Real,1} = Real[], #ordered from lowest to highest temperature
+        COP_curve_points_COP_kw_per_kw::Union{Real,Array{<:Real,1}} = Real[], #one-to-one with COP_curve_points_ambient_temp_degF
         structure_heat_to_ground_mmbtu_per_year::Real=5.9,
         latitude::Float64,
         longitude::Float64
@@ -69,8 +70,8 @@ struct Thermosyphon <: AbstractTech
 
         # validate inputs
         invalid_args = String[]
-        # TODO: enforce length of temp same as loa, or resample
-        # if !(length(ambient_temp_degF))
+        if !isempty(ambient_temp_degF) && length(ambient_temp_degF) != 8760
+            push!(invalid_args, "if ambient_temp_degF is provided it's length must be 8760, got length of $(length(ambient_temp_degF))")
         if !(0 <= effective_conductance_btu_per_degF)
             push!(invalid_args, "effective_conductance_btu_per_degF must satisfy 0 <= effective_conductance_btu_per_degF, got $(effective_conductance_btu_per_degF)")
         end
@@ -80,74 +81,58 @@ struct Thermosyphon <: AbstractTech
         if !(0 <= structure_heat_to_ground_mmbtu_per_year)
             push!(invalid_args, "structure_heat_to_ground_mmbtu_per_year must satisfy 0 <= structure_heat_to_ground_mmbtu_per_year, got $(structure_heat_to_ground_mmbtu_per_year)")
         end
-        if length(COP_curve_points_coefficient_of_performance_kw_per_kw) != length(COP_curve_points_ambient_temp_degF)
-            push!(invalid_args, "COP_curve_points_coefficient_of_performance_kw_per_kw and COP_curve_points_ambient_temp_degF must be the same length, got $(length(COP_curve_points_coefficient_of_performance_kw_per_kw)) and $(length(COP_curve_points_ambient_temp_degF))")
+        if COP_curve_points_COP_kw_per_kw <: Real
+            COP_curve_points_COP_kw_per_kw = [COP_curve_points_COP_kw_per_kw]
+            if isempty(COP_curve_points_ambient_temp_degF)
+                COP_curve_points_ambient_temp_degF = Real[0] #does't matter what value is in here because with only one point the COP curve is a flat line
+            end
+        end
+        if length(COP_curve_points_COP_kw_per_kw) != length(COP_curve_points_ambient_temp_degF)
+            push!(invalid_args, "COP_curve_points_COP_kw_per_kw and COP_curve_points_ambient_temp_degF must be the same length, got $(length(COP_curve_points_COP_kw_per_kw)) and $(length(COP_curve_points_ambient_temp_degF))")
         end
         if length(invalid_args) > 0
             error("Invalid argument values: $(invalid_args)")
         end
 
         if isempty(ambient_temp_degF)
-            # TODO: replace placeholder with temp from PVwatts (convert from python code commented out below)
-            # ambient_temp_degF = repeat([35,5,25,50,60,10,40,90,30,70,30,0],outer=730)
             # Call PVWatts for hourly dry-bulb outdoor air temperature
-            ambient_temp_degF = ambient_temp(latitude, longitude)# .- 10
-            #@info ambient_temp_degF
+            ambient_temp_degF = ambient_temp(latitude, longitude)
         end
 
         if fixed_active_cooling_rate_kw > 0
-            mmbtu_in_kwh = 0.00341214
-
             # Calculate passive cooling (only for time steps T_amb < cutoff temp)
             passive_cooling = ((ground_temp_degF .- filter(t -> t <= passive_to_active_cutoff_temp_degF, ambient_temp_degF)) .* (effective_conductance_btu_per_degF / 1000000))
-            # passive_cooling = ((ground_temp_degF .- replace(t -> t <= passive_to_active_cutoff_temp_degF ? ground_temp_degF : t, ambient_temp_degF)) .* (effective_conductance_btu_per_degF / 1000000))
-            # @info sum(passive_cooling)
+
             # Calculate annual cooling requirement
             min_annual_active_cooling_mmbtu = max(0,structure_heat_to_ground_mmbtu_per_year - sum(passive_cooling))
-            time_steps_monthly = monthly_timesteps(2022)
-            min_monthly_active_cooling_mmbtu = zeros(12)
-            for month in 1:12
-                month_passive_cooling = sum((ground_temp_degF .- filter(t -> t <= passive_to_active_cutoff_temp_degF, [ambient_temp_degF[ts] for ts in time_steps_monthly[month]])) .* (effective_conductance_btu_per_degF / 1000000))
-                # min_monthly_active_cooling_mmbtu[month] = min_annual_active_cooling_mmbtu/12 - month_passive_cooling
-                min_monthly_active_cooling_mmbtu[month] = 0
-            end
-            # month_days = [(0,30), (31,58), (59,89), (90,119), (120,150), (151,180), (181,211), (212,242), (243,272), (273,303), (304,333), (334,364)]
-            # for month in 1:length(month_days)
-            #     month_ts_bounds = month_days[month] .* 24 .+ 1
-            #     month_ts
-            #     min_monthly_active_cooling_mmbtu[month] = structure_heat_to_ground_mmbtu_per_year/12 - sum(passive_cooling[])
 
             # Calculate COP timeseries: could also interpolate linearly outside where we have data
             if isempty(COP_curve_points_ambient_temp_degF)
                 COP_curve_points_ambient_temp_degF = [46,52,63]
-                COP_curve_points_coefficient_of_performance_kw_per_kw = [9,6,3]
+                COP_curve_points_COP_kw_per_kw = [9,6,3]
             end
             coefficient_of_performance_series_mmbtu_per_kwh = zeros(length(ambient_temp_degF))
             for i in 1:length(coefficient_of_performance_series_mmbtu_per_kwh)
                 temp = ambient_temp_degF[i] #< 45 ? 45 : ambient_temp_degF[i]
                 curve_point_temp_is_LTOET = searchsortedfirst(COP_curve_points_ambient_temp_degF, temp)
                 if curve_point_temp_is_LTOET == 1
-                    coefficient_of_performance_series_mmbtu_per_kwh[i] = mmbtu_in_kwh * COP_curve_points_coefficient_of_performance_kw_per_kw[1]
+                    coefficient_of_performance_series_mmbtu_per_kwh[i] = COP_curve_points_COP_kw_per_kw[1] / KWH_PER_MMBTU
                 elseif curve_point_temp_is_LTOET > length(COP_curve_points_ambient_temp_degF)
-                    coefficient_of_performance_series_mmbtu_per_kwh[i] = mmbtu_in_kwh * COP_curve_points_coefficient_of_performance_kw_per_kw[curve_point_temp_is_LTOET-1]
+                    coefficient_of_performance_series_mmbtu_per_kwh[i] = COP_curve_points_COP_kw_per_kw[curve_point_temp_is_LTOET-1] / KWH_PER_MMBTU
                 else
-                    coefficient_of_performance_series_mmbtu_per_kwh[i] = mmbtu_in_kwh * 
-                            (COP_curve_points_coefficient_of_performance_kw_per_kw[curve_point_temp_is_LTOET] 
+                    coefficient_of_performance_series_mmbtu_per_kwh[i] = (COP_curve_points_COP_kw_per_kw[curve_point_temp_is_LTOET] 
                               - (
                                     (COP_curve_points_ambient_temp_degF[curve_point_temp_is_LTOET] - temp)
-                                    * (COP_curve_points_coefficient_of_performance_kw_per_kw[curve_point_temp_is_LTOET] - COP_curve_points_coefficient_of_performance_kw_per_kw[curve_point_temp_is_LTOET-1])
+                                    * (COP_curve_points_COP_kw_per_kw[curve_point_temp_is_LTOET] - COP_curve_points_COP_kw_per_kw[curve_point_temp_is_LTOET-1])
                                     / (COP_curve_points_ambient_temp_degF[curve_point_temp_is_LTOET] - COP_curve_points_ambient_temp_degF[curve_point_temp_is_LTOET-1])
                                 )
-                            )
+                            ) / KWH_PER_MMBTU
                 end
             end
             coefficient_of_performance_series_mmbtu_per_kwh = round.(coefficient_of_performance_series_mmbtu_per_kwh, digits=8)
             
             # Calculate const active cooling rate:
-            active_cooling_rate_mmbtu_per_hour = mmbtu_in_kwh * fixed_active_cooling_rate_kw
-            # alt: Calculate timeseries of what active cooling rate would be at that ambient temp:
-            # active_cooling_rate_to_amb_temp_ratio_kw_per_degF = 0.005238984 #0.006375615
-            # active_cooling_rate_mmbtu_per_hour = mmbtu_in_kwh * active_cooling_rate_to_amb_temp_ratio_kw_per_degF * ambient_temp_degF
+            active_cooling_rate_mmbtu_per_hour = fixed_active_cooling_rate_kw / KWH_PER_MMBTU
            
             # Calculate timesteps where active cooling is and isn't possible:
             time_steps_can_actively_cool = findall(>(passive_to_active_cutoff_temp_degF), ambient_temp_degF)
@@ -186,7 +171,6 @@ function ambient_temp(latitude::Real, longitude::Real; timeframe="hourly")
         end
         @info "PVWatts success."
         amb_temp_f = get(response["outputs"], "tamb", []) .* 1.8 .+ 32.0
-        # watts = collect(get(response["outputs"], "ac", []) / 1000)  # scale to 1 kW system (* 1 kW / 1000 W)
         if length(amb_temp_f) != 8760
             @error "PVWatts did not return a valid ambient temperature. Got $amb_temp_f"
         end
@@ -194,25 +178,4 @@ function ambient_temp(latitude::Real, longitude::Real; timeframe="hourly")
     catch e
         @error "Error occurred when calling PVWatts: $e"
     end
-end
-
-"""
-    monthly_timesteps(year::Int; time_steps_per_hour=1)
-
-return Array{Array{Int64,1},1}, size = (12,)
-"""
-function monthly_timesteps(year::Int; time_steps_per_hour=1)
-    a = Array[]
-    i = 1
-    for m in range(1, stop=12)
-        n_days = daysinmonth(Date(string(year) * "-" * string(m)))
-        stop = n_days * 24 * time_steps_per_hour + i - 1
-        if m == 2 && isleapyear(year)
-            stop -= 24 * time_steps_per_hour  # TODO support extra day in leap years?
-        end
-        steps = [step for step in range(i, stop=stop)]
-        append!(a, [steps])
-        i = stop + 1
-    end
-    return a
 end
