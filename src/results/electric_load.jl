ee1a4cb43bdfd59da98d42d5b05608c6880ca6e6
adfarth <adfarth@umich.edu>
2022-06-22 08:20:20 -0600

    Update electric_load.jl

--- src/results/electric_load.jl
+++ src/results/electric_load.jl
@@ -41,7 +41,7 @@
 """
     add_electric_load_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
 
-Adds the ElectricLoad results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
+Adds the `ElectricLoad` results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
 Note: the node number is an empty string if evaluating a single `Site`.
 """
 function add_electric_load_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
