e0573e7e23428de7e7d961d925d280ee6b117d71
Nick Laws <nick.laws@nrel.gov>
2022-06-27 16:17:46 -0600

    add MPC FlexibleHVAC results

--- src/results/flexible_hvac.jl
+++ src/results/flexible_hvac.jl
@@ -60,10 +60,24 @@ function add_flexible_hvac_results(m::JuMP.AbstractModel, p::REoptInputs{Scenari
 	nothing
 end
 
+
 function add_flexible_hvac_results(m::JuMP.AbstractModel, p::REoptInputs{BAUScenario}, d::Dict; _n="")
     r = Dict{String, Any}()
 
     r["temperatures_degC_node_by_time"] = m[Symbol("dvTemperature"*_n)]
+
+    d["FlexibleHVAC"] = r
+	nothing
+end
+
+
+function add_flexible_hvac_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict; _n="")
+    r = Dict{String, Any}()
+
+    if any(value.(m[:lower_comfort_slack]) .>= 1.0) || any(value.(m[:upper_comfort_slack]) .>= 1.0)
+        @warn "The comfort limits were violated by at least one degree Celcius to keep the problem feasible."
+    end
+    r["temperatures_degC_node_by_time"] = value.(m[Symbol("dvTemperature"*_n)]).data
 
     d["FlexibleHVAC"] = r
 	nothing
