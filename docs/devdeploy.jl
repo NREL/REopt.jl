9c8d964bebe85f93a65af4ad4d425a4784bd63e6
adfarth <adfarth@umich.edu>
2022-06-28 14:26:14 -0600

    README for using devdeploy.jl

--- docs/devdeploy.jl
+++ docs/devdeploy.jl
@@ -2,19 +2,18 @@ using LiveServer
 
 """
 Run this script to locally host 
-NOTE you have to dev REopt in the docs environment to get local changes. 
+NOTE you have to dev REopt in the `docs` environment to get local changes. 
 
 e.g. 
 ```julia
-julia> cd("docs")
+[~/.julia/dev/REopt/docs]
 (REopt) pkg> activate .
 (REopt) pkg> dev REopt
-(docs) pkg> st
-      Status `~/.julia/dev/REopt/docs/Project.toml`
-  [e30172f5] Documenter v0.26.3
-  [4076af6c] JuMP v1.1.1
-  [16fef848] LiveServer v0.8.3
-  [d36ad4e8] REopt v0.16.2 `~/.julia/dev/REopt`
+julia> include("devdeploy.jl")
+[ Info: Precompiling REopt [d36ad4e8-d74a-4f7a-ace1-eaea049febf6]
+...
+✓ LiveServer listening on http://localhost:8000/ ...
+  (use CTRL+C to shut down)
 ```
 """
 function devbuildserve()
