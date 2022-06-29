d8a854198efb19e197697aac4f84cca9da6bc693
zolanaj <alexander.zolan@nrel.gov>
2022-06-29 16:00:32 -0600

    Add MPC TES results functions

--- src/results/thermal_storage.jl
+++ src/results/thermal_storage.jl
@@ -64,6 +64,25 @@ function add_hot_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict,
 end
 
 """
+    add_hot_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict; _n="")
+
+Adds the Storage results to the dictionary passed back from `run_mpc` using the solved model `m` and the `MPCInputs` for node `_n`.
+Note: the node number is an empty string if evaluating a single `Site`.
+
+Storage results:
+- `soc_series_pct` Vector of normalized (0-1) state of charge values over the time horizon [-]
+"""
+function add_hot_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict, b::String; _n="")
+    r = Dict{String, Any}()
+
+    soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
+    r["soc_series_pct"] = round.(value.(soc) ./ p.s.storage.attr[b].size_kwh, digits=3)
+
+    d[b] = r
+    nothing
+end
+
+"""
     add_cold_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
 
 Adds the Storage results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
@@ -94,6 +113,25 @@ function add_cold_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict
         r["year_one_soc_series_pct"] = []
         r["year_one_to_load_series_ton"] = []
     end
+
+    d[b] = r
+    nothing
+end
+
+"""
+    add_cold_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict; _n="")
+
+Adds the Storage results to the dictionary passed back from `run_mpc` using the solved model `m` and the `MPCInputs` for node `_n`.
+Note: the node number is an empty string if evaluating a single `Site`.
+
+Storage results:
+- `soc_series_pct` Vector of normalized (0-1) state of charge values over the time horizon [-]
+"""
+function add_cold_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict, b::String; _n="")
+    r = Dict{String, Any}()
+
+    soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
+    r["soc_series_pct"] = round.(value.(soc) ./ p.s.storage.attr[b].size_kwh, digits=3)
 
     d[b] = r
     nothing
