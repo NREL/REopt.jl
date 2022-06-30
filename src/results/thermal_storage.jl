62e8851cf96fc5c1d5bb8494ed42cbdeac8453a9
adfarth <adfarth@umich.edu>
2022-06-30 14:38:49 -0600

    Update thermal_storage.jl

--- src/results/thermal_storage.jl
+++ src/results/thermal_storage.jl
@@ -62,15 +62,14 @@ function add_hot_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict,
 end
 
 """
-    add_hot_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict; _n="")
-
-Adds the Storage results to the dictionary passed back from `run_mpc` using the solved model `m` and the `MPCInputs` for node `_n`.
-Note: the node number is an empty string if evaluating a single `Site`.
-
-Storage results:
+MPC `HotThermalStorage` results keys:
 - `soc_series_pct` Vector of normalized (0-1) state of charge values over the time horizon [-]
 """
 function add_hot_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict, b::String; _n="")
+    #=
+    Adds the Storage results to the dictionary passed back from `run_mpc` using the solved model `m` and the `MPCInputs` for node `_n`.
+    Note: the node number is an empty string if evaluating a single `Site`.
+    =#
     r = Dict{String, Any}()
 
     soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
@@ -81,19 +80,16 @@ function add_hot_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict, b
 end
 
 """
-    add_cold_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
-
-Adds the HotThermalStorage results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
-Note: the node number is an empty string if evaluating a single `Site`.
-
-Storage results:
+`ColdThermalStorage` results:
 - `size_gal` Optimal TES capacity, by volume [gal]
 - `year_one_soc_series_pct` Vector of normalized (0-1) state of charge values over the first year [-]
 - `year_one_to_load_series_ton` Vector of power used to meet load over the first year [ton]
 """
 function add_cold_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict, b::String; _n="")
-    # Adds the `ColdThermalStorage` results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
-    # Note: the node number is an empty string if evaluating a single `Site`.
+    #=
+    Adds the `ColdThermalStorage` results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
+    Note: the node number is an empty string if evaluating a single `Site`.
+    =#
 
     delta_T_degF = p.s.storage.attr["ColdThermalStorage"].hot_water_temp_degF - p.s.storage.attr["ColdThermalStorage"].cool_water_temp_degF
     avg_cp_kj_per_kgK = 998.2 
@@ -120,15 +116,14 @@ function add_cold_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict
 end
 
 """
-    add_cold_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict; _n="")
-
-Adds the ColdThermalStorage results to the dictionary passed back from `run_mpc` using the solved model `m` and the `MPCInputs` for node `_n`.
-Note: the node number is an empty string if evaluating a single `Site`.
-
-Storage results:
+MPC `ColdThermalStorage` results keys:
 - `soc_series_pct` Vector of normalized (0-1) state of charge values over the time horizon [-]
 """
 function add_cold_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Dict, b::String; _n="")
+    #= 
+    Adds the ColdThermalStorage results to the dictionary passed back from `run_mpc` using the solved model `m` and the `MPCInputs` for node `_n`.
+    Note: the node number is an empty string if evaluating a single `Site`.
+    =#
     r = Dict{String, Any}()
 
     soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
