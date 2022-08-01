3e2ce9a4d7950dd71287654d9bbd18fbcb3a5715
Bill Becker <William.Becker@nrel.gov>
2022-08-01 12:09:51 -0600

    Add tests for addressable load fraction
    Also change name of "Thermal loads" to be more specific to what is being tested

--- test/test_with_xpress.jl
+++ test/test_with_xpress.jl
@@ -32,22 +32,38 @@ using Random
 using DelimitedFiles
 Random.seed!(42)  # for test consistency, random prices used in FlexibleHVAC tests
 
-@testset "Thermal loads" begin
+@testset "Heating loads and addressable load fraction" begin
+    # Default LargeOffice CRB with SpaceHeatingLoad and DomesticHotWaterLoad are served by ExistingBoiler
     m = Model(optimizer_with_attributes(Xpress.Optimizer, "OUTPUTLOG" => 0))
     results = run_reopt(m, "./scenarios/thermal_load.json")
-
     @test round(results["ExistingBoiler"]["year_one_fuel_consumption_mmbtu"], digits=0) ≈ 2905
     
+    # Hourly fuel load inputs with addressable_load_fraction are served as expected
     data = JSON.parsefile("./scenarios/thermal_load.json")
     data["DomesticHotWaterLoad"]["fuel_loads_mmbtu_per_hour"] = repeat([0.5], 8760)
+    data["DomesticHotWaterLoad"]["addressable_load_fraction"] = 0.6
     data["SpaceHeatingLoad"]["fuel_loads_mmbtu_per_hour"] = repeat([0.5], 8760)
+    data["SpaceHeatingLoad"]["addressable_load_fraction"] = 0.8
     s = Scenario(data)
     inputs = REoptInputs(s)
     m = Model(optimizer_with_attributes(Xpress.Optimizer, "OUTPUTLOG" => 0))
     results = run_reopt(m, inputs)
+    @test round(results["ExistingBoiler"]["year_one_fuel_consumption_mmbtu"], digits=0) ≈ 8760 * (0.5 * 0.6 + 0.5 * 0.8)
+    
+    # Monthly fuel load input with addressable_load_fraction is processed to expected thermal load
+    data = JSON.parsefile("./scenarios/thermal_load.json")
+    data["DomesticHotWaterLoad"]["monthly_mmbtu"] = repeat([100], 12)
+    data["DomesticHotWaterLoad"]["addressable_load_fraction"] = repeat([0.6], 12)
+    data["SpaceHeatingLoad"]["monthly_mmbtu"] = repeat([200], 12)
+    data["SpaceHeatingLoad"]["addressable_load_fraction"] = repeat([0.8], 12)
 
-    @test round(results["ExistingBoiler"]["year_one_fuel_consumption_mmbtu"], digits=0) ≈ 8760
-    # TODO chiller tests
+    s = Scenario(data)
+    inputs = REoptInputs(s)
+
+    dhw_thermal_load_expected = sum(data["DomesticHotWaterLoad"]["monthly_mmbtu"] .* data["DomesticHotWaterLoad"]["addressable_load_fraction"]) .* s.existing_boiler.efficiency
+    space_thermal_load_expected = sum(data["SpaceHeatingLoad"]["monthly_mmbtu"] .* data["SpaceHeatingLoad"]["addressable_load_fraction"]) .* s.existing_boiler.efficiency
+    @test round(sum(s.dhw_load.loads_kw) / REopt.KWH_PER_MMBTU) ≈ sum(dhw_thermal_load_expected)
+    @test round(sum(s.space_heating_load.loads_kw) / REopt.KWH_PER_MMBTU) ≈ sum(space_thermal_load_expected)
 end
 
 @testset "CHP" begin
