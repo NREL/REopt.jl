44b25ab3c5b8a422e740b80e678690fa3efa95d4
Bill Becker <William.Becker@nrel.gov>
2022-08-02 15:29:40 -0600

    Remove CSV and DataFrames from test environment
    Change CSV->DataFrames functionality to DelimitedFiles

--- test/test_with_xpress.jl
+++ test/test_with_xpress.jl
@@ -1003,10 +1003,6 @@ end
     3. GHP serves only the SpaceHeatingLoad by default unless it is allowed to serve DHW
     4. GHP serves all the Cooling load
     5. Input of a custom COP map for GHP and check the GHP performance to make sure it's using it correctly
-
-    TODO once these separate features are added to REopt.jl:
-    6. Alternative schedule-based FlatLoad profiles are used
-    7. "Addressable load fraction" reduces the total heating load relative to the fuel input
     
     """
     # Load base inputs
@@ -1014,21 +1010,21 @@ end
     
     # Modify ["GHP"]["ghpghx_inputs"] for running GhpGhx.jl
     # Heat pump performance maps
-    cop_map_df = CSV.read("scenarios/ghp_cop_map_custom.csv", DataFrame)
+    cop_map_mat_header = readdlm("scenarios/ghp_cop_map_custom.csv", ',', header=true)
+    data = cop_map_mat_header[1]
+    headers = cop_map_mat_header[2]
     # Generate a "records" style dictionary from the 
     cop_map_list = []
-    for i in eachrow(cop_map_df)
-        dict_record = Dict(names(i)[k]=>i[names(i)[k]] for k in 1:length(names(i)))
+    for i in 1:length(data[:,1])
+        dict_record = Dict(name=>data[i, col] for (col, name) in enumerate(headers))
         push!(cop_map_list, dict_record)
     end
     input_data["GHP"]["ghpghx_inputs"][1]["cop_map_eft_heating_cooling"] = cop_map_list
     
     # Could also load in a "ghpghx_responses" which is combined ghpghx_inputs (but processed to populate all), and ghpghx_results
-    input_data["GHP"]["ghpghx_responses"] = [JSON.parsefile("scenarios/ghpghx_response.json")]
+    #input_data["GHP"]["ghpghx_responses"] = [JSON.parsefile("scenarios/ghpghx_response.json")]
     
     # Heating load
-    # TODO "alternative" schedule-based loads are not currently available in REopt.jl
-    # TODO neither is handling flat loads for space heating and domestic hot water (cooling has it) - don't need .dat files!
     input_data["SpaceHeatingLoad"]["doe_reference_name"] = "Hospital"
     input_data["SpaceHeatingLoad"]["monthly_mmbtu"] = fill(1000.0, 12)
     input_data["SpaceHeatingLoad"]["monthly_mmbtu"][1] = 500.0
@@ -1045,12 +1041,12 @@ end
     
     # Test GHP serving addressable fraction of space heating with VAV efficiency thermal knockdown
     heating_served_mmbtu = sum(s.ghp_option_list[ghp_option_chosen].heating_thermal_kw / REopt.KWH_PER_MMBTU)
-    expected_heating_served_mmbtu = 12000 * 0.8 * 0.85  # (fuel_mmbtu * boiler_effic * (TBD * addressable_load) * space_heating_efficiency_thermal_factor)
+    expected_heating_served_mmbtu = 12000 * 0.8 * 0.85  # (fuel_mmbtu * boiler_effic * space_heating_efficiency_thermal_factor)
     @test round(heating_served_mmbtu, digits=1) ≈ expected_heating_served_mmbtu atol=1.0
     
     # Boiler serves all of the DHW load, no DHW thermal reduction due to GHP retrofit
     boiler_served_mmbtu = sum(results["ExistingBoiler"]["year_one_thermal_production_mmbtu_per_hour"])
-    expected_boiler_served_mmbtu = 3000 * 0.8 #* 0.9 (fuel_mmbtu * boiler_effic * (TBD addressable_load_frac))
+    expected_boiler_served_mmbtu = 3000 * 0.8 #* 0.9 (fuel_mmbtu * boiler_effic)
     @test round(boiler_served_mmbtu, digits=1) ≈ expected_boiler_served_mmbtu atol=1.0
     
     # LoadProfileChillerThermal cooling thermal is 1/cooling_efficiency_thermal_factor of GHP cooling thermal production
