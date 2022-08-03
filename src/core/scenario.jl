f88e5ef353ef3b78b5e022786ae287df154b5588
Bill Becker <William.Becker@nrel.gov>
2022-08-03 09:07:59 -0600

    Update Scenario docstring for GHP

--- src/core/scenario.jl
+++ src/core/scenario.jl
@@ -71,9 +71,9 @@ A Scenario struct can contain the following keys:
 - [FlexibleHVAC](@ref) (optional)
 - [ExistingChiller](@ref) (optional)
 - [AbsorptionChiller](@ref) (optional)
-- [GHP](@ref) (optional)
+- [GHP](@ref) (optional, can be Array)
 
-All values of `d` are expected to be `Dicts` except for `PV`, which can be either a `Dict` or `Dict[]` (for multiple PV arrays).
+All values of `d` are expected to be `Dicts` except for `PV` and `GHP`, which can be either a `Dict` or `Dict[]` (for multiple PV arrays or GHP options).
 
 !!! note 
     Set `flex_hvac_from_json=true` if `FlexibleHVAC` values were loaded in from JSON (necessary to 
