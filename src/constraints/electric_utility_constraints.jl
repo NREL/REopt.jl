6c9f6f5db4b25966e3e4546589cf2e39959a4f50
hdunham <70401017+hdunham@users.noreply.github.com>
2022-12-06 15:11:17 -0700

    fix lookback month wrap around year bug

--- src/constraints/electric_utility_constraints.jl
+++ src/constraints/electric_utility_constraints.jl
@@ -298,7 +298,7 @@ function add_demand_lookback_constraints(m, p; _n="")
 	if p.s.electric_tariff.demand_lookback_range != 0  # then the dvPeakDemandLookback varies by month
 
 		##Constraint (12e): dvPeakDemandLookback is the highest peak demand in DemandLookbackMonths
-        @constraint(m, [mth in p.months, lm in 1:p.s.electric_tariff.demand_lookback_range, ts in p.s.electric_tariff.time_steps_monthly[mod(mth - lm, 12)]],
+        @constraint(m, [mth in p.months, lm in 1:p.s.electric_tariff.demand_lookback_range, ts in p.s.electric_tariff.time_steps_monthly[mod(mth - lm - 1, 12) + 1]],
             m[Symbol(dv)][mth] ≥ sum( m[Symbol("dvGridPurchase"*_n)][ts, tier] 
                                         for tier in 1:p.s.electric_tariff.n_energy_tiers )
         )
