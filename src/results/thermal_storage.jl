6b59804c684122b411bed6276a0e4ea0efc4eb0b
Bill Becker <William.Becker@nrel.gov>
2022-12-23 11:42:31 -0700

    Use CoolProp to look up water properties and round ThermalStorage sizes (gal)

--- src/results/thermal_storage.jl
+++ src/results/thermal_storage.jl
@@ -38,13 +38,14 @@ function add_hot_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict,
     # Note: the node number is an empty string if evaluating a single `Site`.
 
     delta_T_degF = p.s.storage.attr["HotThermalStorage"].hot_water_temp_degF - p.s.storage.attr["HotThermalStorage"].cool_water_temp_degF
-    avg_cp_kj_per_kgK = 998.2 
-    avg_rho_kg_per_m3 = 4.184 #TODO: add CoolProp reference or perform analogous calculations for water and build lookup tables
+    avg_T_degF = (p.s.storage.attr["HotThermalStorage"].hot_water_temp_degF + p.s.storage.attr["HotThermalStorage"].cool_water_temp_degF) / 2.0
+    avg_rho_kg_per_m3 = PropsSI("D","P",101325,"T",convert_temp_degF_to_Kelvin(avg_T_degF),"Water")
+    avg_cp_kj_per_kgK = PropsSI("CPMASS","P",101325,"T",convert_temp_degF_to_Kelvin(avg_T_degF),"Water")/1000  # kJ/kg-K
     kwh_per_gal = convert_gal_to_kwh(delta_T_degF, avg_rho_kg_per_m3, avg_cp_kj_per_kgK)
     
     r = Dict{String, Any}()
-    size_kwh = round(value(m[Symbol("dvStorageEnergy"*_n)][b]), digits=2)
-    r["size_gal"] = size_kwh / kwh_per_gal
+    size_kwh = round(value(m[Symbol("dvStorageEnergy"*_n)][b]), digits=3)
+    r["size_gal"] = round(size_kwh / kwh_per_gal, digits=0)
 
     if size_kwh != 0
     	soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
@@ -92,13 +93,14 @@ function add_cold_storage_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict
     =#
 
     delta_T_degF = p.s.storage.attr["ColdThermalStorage"].hot_water_temp_degF - p.s.storage.attr["ColdThermalStorage"].cool_water_temp_degF
-    avg_cp_kj_per_kgK = 998.2 
-    avg_rho_kg_per_m3 = 4.184 #TODO: add CoolProp reference or perform analogous calculations for water and build lookup tables
+    avg_T_degF = (p.s.storage.attr["ColdThermalStorage"].hot_water_temp_degF + p.s.storage.attr["ColdThermalStorage"].cool_water_temp_degF) / 2.0
+    avg_rho_kg_per_m3 = PropsSI("D","P",101325,"T",convert_temp_degF_to_Kelvin(avg_T_degF),"Water")
+    avg_cp_kj_per_kgK = PropsSI("CPMASS","P",101325,"T",convert_temp_degF_to_Kelvin(avg_T_degF),"Water")/1000  # kJ/kg-K
     kwh_per_gal = convert_gal_to_kwh(delta_T_degF, avg_rho_kg_per_m3, avg_cp_kj_per_kgK)
     
     r = Dict{String, Any}()
-    size_kwh = round(value(m[Symbol("dvStorageEnergy"*_n)][b]), digits=2)
-    r["size_gal"] = size_kwh / kwh_per_gal
+    size_kwh = round(value(m[Symbol("dvStorageEnergy"*_n)][b]), digits=3)
+    r["size_gal"] = round(size_kwh / kwh_per_gal, digits=0)
 
     if size_kwh != 0
     	soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
