c502a8bfa220bffa59425cf5e661e4eb70fdce0a
Bill Becker <William.Becker@nrel.gov>
2022-12-23 11:52:05 -0700

    Change ThermalStorage expected sizes in test based on:
    Updated (more accurate) water property value from CoolProp

--- test/test_with_xpress.jl
+++ test/test_with_xpress.jl
@@ -736,8 +736,8 @@ end
     @test sum(r["HotThermalStorage"]["year_one_to_load_series_mmbtu_per_hour"]) ≈ 149.45 atol=0.1
     @test sum(r["ColdThermalStorage"]["year_one_to_load_series_ton"]) ≈ 12454.33 atol=0.1
     #size should be just over 10kW in gallons, accounting for efficiency losses and min SOC
-    @test r["HotThermalStorage"]["size_gal"] ≈ 227.89 atol=0.1
-    @test r["ColdThermalStorage"]["size_gal"] ≈ 379.82 atol=0.1
+    @test r["HotThermalStorage"]["size_gal"] ≈ 233.0 atol=0.1
+    @test r["ColdThermalStorage"]["size_gal"] ≈ 378.0 atol=0.1
     #No production from existing chiller, only absorption chiller, which is sized at ~5kW to manage electric demand charge & capital cost.
     @test r["ExistingChiller"]["year_one_thermal_production_tonhour"] ≈ 0.0 atol=0.1
     @test r["AbsorptionChiller"]["year_one_thermal_production_tonhour"] ≈ 12464.15 atol=0.1
