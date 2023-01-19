4d033b791abe62dc856e19299d0edc31c7b10afa
zolanaj <alexander.zolan@nrel.gov>
2023-01-19 14:27:47 -0700

    add check for sum of outage probabilities = 1.0

--- src/core/electric_utility.jl
+++ src/core/electric_utility.jl
@@ -210,6 +210,9 @@ struct ElectricUtility
         if length(outage_durations) != length(outage_probabilities)
             throw(@error("ElectricUtility inputs outage_durations and outage_probabilities must be the same length"))
         end
+        if sum(outage_probabilities) < 0.99999 || sum(outage_probabilities) > 1.00001
+            throw(@error("Sum of ElectricUtility inputs outage_probabilities must be equal to 1"))
+        end
 
         new(
             is_MPC ? "" : emissions_region,
