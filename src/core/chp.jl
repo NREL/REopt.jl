ff17949287b251b98b65cc14e08b5133819afca7
bpulluta <byron.pullutasig@nrel.gov>
2023-01-23 10:57:57 -0700

    Updated Federal Incentives (PV, Wind, CHP, GHP -> 30 percent)  and MACRS bonus depreciation (ALL-> 80 percent)

--- src/core/chp.jl
+++ src/core/chp.jl
@@ -64,7 +64,7 @@ prime_movers = ["recip_engine", "micro_turbine", "combustion_turbine", "fuel_cel
     can_supply_steam_turbine::Bool=false
 
     macrs_option_years::Int = 5
-    macrs_bonus_fraction::Float64 = 1.0
+    macrs_bonus_fraction::Float64 = 0.8
     macrs_itc_reduction::Float64 = 0.5
     federal_itc_fraction::Float64 = 0.3
     federal_rebate_per_kw::Float64 = 0.0
@@ -131,7 +131,7 @@ Base.@kwdef mutable struct CHP <: AbstractCHP
     can_supply_steam_turbine::Bool=false
 
     macrs_option_years::Int = 5
-    macrs_bonus_fraction::Float64 = 1.0
+    macrs_bonus_fraction::Float64 = 0.8
     macrs_itc_reduction::Float64 = 0.5
     federal_itc_fraction::Float64 = 0.3
     federal_rebate_per_kw::Float64 = 0.0
--- src/core/ghp.jl
+++ src/core/ghp.jl
@@ -52,7 +52,7 @@ struct with outer constructor:
     can_serve_dhw::Bool = false
 
     macrs_option_years::Int = 5
-    macrs_bonus_fraction::Float64 = 1.0
+    macrs_bonus_fraction::Float64 = 0.8
     macrs_itc_reduction::Float64 = 0.5
     federal_itc_fraction::Float64 = 0.3
     federal_rebate_per_ton::Float64 = 0.0
@@ -98,7 +98,7 @@ Base.@kwdef mutable struct GHP <: AbstractGHP
     can_serve_dhw::Bool = false
 
     macrs_option_years::Int = 5
-    macrs_bonus_fraction::Float64 = 1.0
+    macrs_bonus_fraction::Float64 = 0.8
     macrs_itc_reduction::Float64 = 0.5
     federal_itc_fraction::Float64 = 0.3
     federal_rebate_per_ton::Float64 = 0.0
