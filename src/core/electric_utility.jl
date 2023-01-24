d42e2075fa76a714a8c8e4553dc5d5a967bbe7b0
adfarth <adfarth@umich.edu>
2023-01-24 09:29:06 -0700

    modify descriptions, remove inputs that users should not provide

--- src/core/electric_utility.jl
+++ src/core/electric_utility.jl
@@ -31,7 +31,7 @@
 `ElectricUtility` is an optional REopt input with the following keys and default values:
 ```julia
     net_metering_limit_kw::Real = 0,
-    interconnection_limit_kw::Real = 1.0e9, # Limit on system capacity size that can be interconnected to the grid 
+    interconnection_limit_kw::Real = 1.0e9, # Limit on total electric system capacity size that can be interconnected to the grid 
     outage_start_time_step::Int=0,  # for modeling a single outage, with critical load spliced into the baseline load ...
     outage_end_time_step::Int=0,  # ... utiltity production_factor = 0 during the outage
     allow_simultaneous_export_import::Bool = true,  # if true the site has two meters (in effect)
