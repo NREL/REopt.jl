c125d12c6b49fdfe9ad91a32c4383722dd89db3d
Bill Becker <William.Becker@nrel.gov>
2023-02-08 20:23:52 -0700

    Export function assign_thermal_factor! for API's ghp_efficiency_thermal_factors

--- src/REopt.jl
+++ src/REopt.jl
@@ -48,7 +48,8 @@ export
     get_chp_defaults_prime_mover_size_class,
     get_steam_turbine_defaults_size_class,
     simulated_load,
-    get_absorption_chiller_defaults
+    get_absorption_chiller_defaults,
+    assign_thermal_factor!
 
 import HTTP
 import JSON
