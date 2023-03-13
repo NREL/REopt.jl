aacec740da320e70bec54cd03b04ab73c178a642
Tucker Oddleifson <tucker.oddleifson@nrel.gov>
2023-03-13 11:39:19 -0600

    Update storage_constraints.jl

--- src/constraints/storage_constraints.jl
+++ src/constraints/storage_constraints.jl
@@ -85,6 +85,8 @@ function add_elec_storage_dispatch_constraints(m, p, b; _n="")
             + p.s.storage.attr[b].grid_charge_efficiency * m[Symbol("dvGridToStorage"*_n)][b, ts] 
             - m[Symbol("dvDischargeFromStorage"*_n)][b,ts] / p.s.storage.attr[b].discharge_efficiency
         )
+            - ((0.01/24)*m[Symbol("dvStoredEnergy"*_n)][b, ts]) # TEO added this to account for 1% thermal loss of LDES; this line currently only works for hourly data
+        
 	)
 
 	# Constraint (4h): state-of-charge for electrical storage - no grid
@@ -93,6 +95,8 @@ function add_elec_storage_dispatch_constraints(m, p, b; _n="")
             sum(p.s.storage.attr[b].charge_efficiency * m[Symbol("dvProductionToStorage"*_n)][b,t,ts] for t in p.techs.elec) 
             - m[Symbol("dvDischargeFromStorage"*_n)][b, ts] / p.s.storage.attr[b].discharge_efficiency
         )
+        - ((0.01/24)*m[Symbol("dvStoredEnergy"*_n)][b, ts]) # TEO added this to account for 1% thermal loss of LDES; this line currently only works for hourly data
+        
     )
 
 	# Constraint (4i)-1: Dispatch to electrical storage is no greater than power capacity
