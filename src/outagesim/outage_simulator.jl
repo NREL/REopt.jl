73727f521a27327e25affc49ff0c9de6faf501e9
hdunham <70401017+hdunham@users.noreply.github.com>
2023-03-30 14:30:54 -0600

    add HHV arg to fuel burn utils function

--- src/outagesim/outage_simulator.jl
+++ src/outagesim/outage_simulator.jl
@@ -293,7 +293,8 @@ function simulate_outages(d::Dict, p::REoptInputs; microgrid_only::Bool=false)
 
 	fuel_slope_gal_per_kwhe, fuel_intercept_gal_per_hr = generator_fuel_slope_and_intercept(
 		electric_efficiency_full_load=p.s.generator.electric_efficiency_full_load, 
-		electric_efficiency_half_load=p.s.generator.electric_efficiency_half_load
+		electric_efficiency_half_load=p.s.generator.electric_efficiency_half_load,
+        fuel_higher_heating_value_kwh_per_gal = p.s.generator.fuel_higher_heating_value_kwh_per_gal
 	)
 
     simulate_outages(;
