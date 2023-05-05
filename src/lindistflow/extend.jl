e5e9ac019e731b03200b299401abc57cf76fa460
Nick Laws <laws.nick@gmail.com>
2023-05-04 21:33:52 -0600

    mv LinDistFlow to runtests.jl (requires SCIP) and upgrade LinDistFlow version to latest
    also added doc strings to src/lindistflow/extend.jl
    
    the LinDistFlow API has changed
    
    could model the SOS constraints with integers to remove SCIP but might want to consider switching some integer constraints in REopt to SOS now that more solvers support them

--- src/lindistflow/extend.jl
+++ src/lindistflow/extend.jl
@@ -1,14 +1,11 @@
 """
-Outline:
-1. Construct LDF.Inputs from openDSS file, load dict
-2. Add power flow constraints to m, 
-    - set Pⱼ's = -1 * (dvGridPurchase_j - dvProductionToGrid_j)
-3. solve model
+    LDF.build_ldf!(m::JuMP.AbstractModel, p::LDF.Inputs, ps::AbstractVector{REoptInputs{Scenario}};
+        make_import_export_complementary::Bool=true
+    )
+
+Override the LinDistFlow build_ldf! method to account for REoptInputs
 """
-# TODO add complementary constraint to UL for dvProductionToGrid_ and dvGridPurchase_ (don't want it in LL s.t. it stays linear)
-
-
-function LDF.build_ldf!(m::JuMP.AbstractModel, p::LDF.Inputs, ps::Array{REoptInputs, 1};
+function LDF.build_ldf!(m::JuMP.AbstractModel, p::LDF.Inputs, ps::AbstractVector{REoptInputs{Scenario}};
         make_import_export_complementary::Bool=true
     )
     LDF.add_variables(m, p)
@@ -17,7 +14,6 @@ function LDF.build_ldf!(m::JuMP.AbstractModel, p::LDF.Inputs, ps::Array{REoptInp
     LDF.constrain_substation_voltage(m, p)
     LDF.constrain_KVL(m, p)
     LDF.constrain_loads(m, p, ps)
-    LDF.constrain_bounds(m, p)
 
     if make_import_export_complementary
         add_complementary_constraints(m, ps)
@@ -25,7 +21,12 @@ function LDF.build_ldf!(m::JuMP.AbstractModel, p::LDF.Inputs, ps::Array{REoptInp
 end
 
 
-function add_expressions(m::JuMP.AbstractModel, ps::Array{REoptInputs, 1})
+"""
+    add_expressions(m::JuMP.AbstractModel, ps::AbstractVector{REoptInputs{Scenario}})
+
+Add the TotalExport_n expressions for use in defining LinDistFlow net injections
+"""
+function add_expressions(m::JuMP.AbstractModel, ps::AbstractVector{REoptInputs{Scenario}})
     for p in ps
         _n = string("_", p.s.site.node)
         m[Symbol("TotalExport"*_n)] = @expression(m, [t in p.time_steps],
@@ -38,7 +39,12 @@ function add_expressions(m::JuMP.AbstractModel, ps::Array{REoptInputs, 1})
 end
 
 
-function add_complementary_constraints(m::JuMP.AbstractModel, ps::Array{REoptInputs, 1})
+"""
+    add_complementary_constraints(m::JuMP.AbstractModel, ps::AbstractVector{REoptInputs{Scenario}})
+
+Constrain one or both of `dvGridPurchase_n` and `TotalExport_n` to be zero.
+"""
+function add_complementary_constraints(m::JuMP.AbstractModel, ps::AbstractVector{REoptInputs{Scenario}})
     for p in ps
         _n = string("_", p.s.site.node)
         for (i, e) in zip(m[Symbol("dvGridPurchase"*_n)], m[Symbol("TotalExport"*_n)])
@@ -50,57 +56,61 @@ function add_complementary_constraints(m::JuMP.AbstractModel, ps::Array{REoptInp
 end
 
 
-function LDF.constrain_loads(m::JuMP.AbstractModel, p::LDF.Inputs, ps::Array{REoptInputs, 1})
+"""
+    LDF.constrain_loads(m::JuMP.AbstractModel, p::LDF.Inputs, ps::AbstractVector{REoptInputs{Scenario}})
+
+Override the `LinDistFlow.constrain_loads` method to set REopt decisions as net load injections
+"""
+function LDF.constrain_loads(m::JuMP.AbstractModel, p::LDF.Inputs, ps::AbstractVector{REoptInputs{Scenario}})
     reopt_nodes = [p.s.site.node for p in ps]
 
-    Pⱼ = m[:Pⱼ]
-    Qⱼ = m[:Qⱼ]
+    Pj = m[:Pj]
+    Qj = m[:Qj]
     # positive values are injections
 
     for j in p.busses
         if j in keys(p.Pload)
             if parse(Int, j) in reopt_nodes
                 @constraint(m, [t in 1:p.Ntimesteps],
-                    Pⱼ[j,t] == 1e3/p.Sbase * (  # 1e3 b/c REopt values in kW
+                    Pj[j,t] == 1e3/p.Sbase * (  # 1e3 b/c REopt values in kW
                         m[Symbol("TotalExport_" * j)][t]
                         - m[Symbol("dvGridPurchase_" * j)][t]
                     )
                 )
             else
                 @constraint(m, [t in 1:p.Ntimesteps],
-                    Pⱼ[j,t] == -p.Pload[j][t]
+                    Pj[j,t] == -p.Pload[j][t]
                 )
             end
         elseif j != p.substation_bus
             @constraint(m, [t in 1:p.Ntimesteps],
-                Pⱼ[j,t] == 0
+                Pj[j,t] == 0
             )
         end
         
         if j in keys(p.Qload)
             if parse(Int, j) in reopt_nodes
                 @constraint(m, [t in 1:p.Ntimesteps],
-                    Qⱼ[j,t] == 1e3/p.Sbase * p.pf * (  # 1e3 b/c REopt values in kW
+                    Qj[j,t] == 1e3/p.Sbase * p.pf * (  # 1e3 b/c REopt values in kW
                     m[Symbol("TotalExport_" * j)][t]
                         - m[Symbol("dvGridPurchase_" * j)][t]
                     )
                 )
             else
                 @constraint(m, [t in 1:p.Ntimesteps],
-                    Qⱼ[j,t] == -p.Qload[j][t]
+                    Qj[j,t] == -p.Qload[j][t]
                 )
             end
         elseif j != p.substation_bus
             @constraint(m, [t in 1:p.Ntimesteps],
-                Qⱼ[j,t] == 0
+                Qj[j,t] == 0
             )
         end
     end
-    p.Nequality_cons += 2 * (p.Nnodes - 1) * p.Ntimesteps
+    nothing
 end
 
+
+# function run_reopt(m::JuMP.AbstractModel, p::REoptInputs, ldf::LDF.Inputs)
 # TODO add LDF results (here and in LDF package)
-
-function run_reopt(m::JuMP.AbstractModel, p::REoptInputs, ldf::LDF.Inputs)
-
-end
+# end
