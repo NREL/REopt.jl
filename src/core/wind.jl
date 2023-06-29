fb3b53f5f713f9eb0f94adcc3262a675eb6c522d
Emika Brown <137338302+emika-brown@users.noreply.github.com>
2023-06-29 16:13:09 -0600

    electric_storage, generator, and wind default changes
    Modifications made to the defaults for installed_cost_per_kw, installed_cost_per_kwh, fuel_cost_per_gallon, and size_class_to_installed_cost dictionary.

--- src/core/wind.jl
+++ src/core/wind.jl
@@ -33,7 +33,7 @@
     min_kw = 0.0,
     max_kw = 1.0e9,
     installed_cost_per_kw = nothing,
-    om_cost_per_kw = 35.0,
+    om_cost_per_kw = 36.0,
     production_factor_series = nothing, # Optional user-defined production factors. Must be normalized to units of kW-AC/kW-DC nameplate. The series must be one year (January through December) of hourly, 30-minute, or 15-minute generation data.
     size_class = "",
     wind_meters_per_sec = [],
@@ -69,10 +69,10 @@
     If no `installed_cost_per_kw` is provided then it is determined from:
     ```julia
     size_class_to_installed_cost = Dict(
-        "residential"=> 11950.0,
-        "commercial"=> 7390.0,
-        "medium"=> 4440.0,
-        "large"=> 3450.0
+        "residential"=> 6339.0,
+        "commercial"=> 4760.0,
+        "medium"=> 3137.0,
+        "large"=> 2386.0
     )
     ```
     If the `production_factor_series` is not provided then NREL's System Advisor Model (SAM) is used to get the wind turbine 
@@ -130,7 +130,7 @@ struct Wind <: AbstractTech
         min_kw = 0.0,
         max_kw = 1.0e9,
         installed_cost_per_kw = nothing,
-        om_cost_per_kw = 35.0,
+        om_cost_per_kw = 36.0,
         production_factor_series = nothing,
         size_class = "",
         wind_meters_per_sec = [],
@@ -169,10 +169,10 @@ struct Wind <: AbstractTech
             "large"=> 80
         )
         size_class_to_installed_cost = Dict(
-            "residential"=> 5675.0,
-            "commercial"=> 4300.0,
-            "medium"=> 2766.0,
-            "large"=> 2239.0
+            "residential"=> 6339.0,
+            "commercial"=> 4760.0,
+            "medium"=> 3137.0,
+            "large"=> 2386.0
         )
         
         if size_class == ""
