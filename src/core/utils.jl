01860edb6da7728c4cc78db6574e645a2a8a931b
Bill Becker <William.Becker@nrel.gov>
2023-07-02 15:24:20 -0600

    Update single PVWatts API function
    For both production factor (PV) and ambient air temperature data (GHP)

--- src/core/utils.jl
+++ src/core/utils.jl
@@ -377,43 +377,15 @@ function generate_year_profile_hourly(year::Int64, consecutive_periods::Abstract
 end
 
 
-function get_ambient_temperature(latitude::Real, longitude::Real)
-    dataset = "nsrdb"
-    if longitude < -179.5 || longitude > -21.0 || latitude < -21.5 || latitude > 60.0
-        if longitude < 81.5 || longitude > 179.5 || latitude < -60.0 || latitude > 60.0 
-            if longitude < 67.0 || latitude < -40.0 || latitude > 38.0
-                dataset = "intl"
-            end
-        end
-    end
-    url = string("https://developer.nrel.gov/api/pvwatts/v8.json", "?api_key=", nrel_developer_key,
-        "&lat=", latitude , "&lon=", longitude, "&tilt=", latitude,
-        "&system_capacity=1", "&azimuth=", 180, "&module_type=", 0,
-        "&array_type=", 0, "&losses=", 14,
-        "&timeframe=hourly", # can only get tamb when timeframe=houly according to PVWatts documentation
-        "&dataset=", dataset 
-    )
+"""
+    call_pvwatts_api(latitude::Real, longitude::Real; timeframe="hourly", azimuth=180, module_type=0, array_type=1, tilt=latitude, time_steps_per_hour=1)
 
-    try
-        @info "Querying PVWatts for ambient temperature... "
-        r = HTTP.get(url)
-        response = JSON.parse(String(r.body))
-        if r.status != 200
-            throw(@error("Bad response from PVWatts: $(response["errors"])"))
-        end
-        @info "PVWatts success."
-        tamb = collect(get(response["outputs"], "tamb", []))  # Celcius
-        if length(tamb) != 8760
-            throw(@error("PVWatts did not return a valid temperature. Got $tamb"))
-        end
-        return tamb
-    catch e
-        throw(@error("Error occurred when calling PVWatts: $e"))
-    end
-end
-
-
-function get_pvwatts_prodfactor(latitude::Real, longitude::Real; timeframe="hourly", azimuth=180, module_type=0, array_type=1, tilt=latitude)
+This calls the PVWatts API and returns both:
+ - PV production factor
+ - Ambient outdoor air dry bulb temperature profile [Celcius] 
+"""
+function call_pvwatts_api(latitude::Real, longitude::Real; tilt=latitude, azimuth=180, module_type=1, array_type=1, 
+    losses=14, dc_ac_ratio=1.2, gcr=0.4, inv_eff=96, timeframe="hourly", radius=0, time_steps_per_hour=1)
     # Check if site is beyond the bounds of the NRSDB TMY dataset. If so, use the international dataset.
     dataset = "nsrdb"
     if longitude < -179.5 || longitude > -21.0 || latitude < -21.5 || latitude > 60.0
@@ -426,23 +398,36 @@ function get_pvwatts_prodfactor(latitude::Real, longitude::Real; timeframe="hour
     url = string("https://developer.nrel.gov/api/pvwatts/v8.json", "?api_key=", nrel_developer_key,
         "&lat=", latitude , "&lon=", longitude, "&tilt=", tilt,
         "&system_capacity=1", "&azimuth=", azimuth, "&module_type=", module_type,
-        "&array_type=", array_type, "&losses=", 14,
-        "&timeframe=", timeframe, "&dataset=", dataset
-    )
+        "&array_type=", array_type, "&losses=", losses, "&dc_ac_ratio=", dc_ac_ratio,
+        "&gcr=", gcr, "&inv_eff=", inv_eff, "&timeframe=", timeframe, "&dataset=", dataset,
+        "&radius=", radius
+        )
 
     try
-        @info "Querying PVWatts for production factor of 1 kW system with tilt set to latitude... "
-        r = HTTP.get(url)
+        @info "Querying PVWatts for production factor and ambient air temperature... "
+        r = HTTP.get(url, keepalive=true, readtimeout=10)
         response = JSON.parse(String(r.body))
         if r.status != 200
             throw(@error("Bad response from PVWatts: $(response["errors"])"))
         end
         @info "PVWatts success."
+        # Get both possible data of interest
         watts = collect(get(response["outputs"], "ac", []) / 1000)  # scale to 1 kW system (* 1 kW / 1000 W)
+        tamb_celcius = collect(get(response["outputs"], "tamb", []))  # Celcius
+        # Validate outputs
         if length(watts) != 8760
             throw(@error("PVWatts did not return a valid prodfactor. Got $watts"))
         end
-        return watts
+        # Validate tamb_celcius
+        if length(tamb_celcius) != 8760
+            throw(@error("PVWatts did not return a valid temperature. Got $tamb_celcius"))
+        end 
+        # Upsample or downsample based on model time_steps_per_hour
+        if time_steps_per_hour > 1
+            watts = repeat(watts, inner=time_steps_per_hour)
+            tamb_celcius = repeat(tamb_celcius, inner=time_steps_per_hour)
+        end
+        return watts, tamb_celcius
     catch e
         throw(@error("Error occurred when calling PVWatts: $e"))
     end
