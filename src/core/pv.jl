da8149cfaf10bf78f02c0b1494b6ee34ed2ebda8
Emika Brown <137338302+emika-brown@users.noreply.github.com>
2023-07-13 10:47:54 -0600

    default update for ground mount tilt in pv.jl
    Updated the default tilt angle for ground mount PV from the site latitude to 25 degrees.

--- src/core/pv.jl
+++ src/core/pv.jl
@@ -31,7 +31,7 @@
 `PV` is an optional REopt input with the following keys and default values:
 ```julia
     array_type::Int=1, # PV Watts array type (0: Ground Mount Fixed (Open Rack); 1: Rooftop, Fixed; 2: Ground Mount 1-Axis Tracking; 3 : 1-Axis Backtracking; 4: Ground Mount, 2-Axis Tracking)
-    tilt::Real= array_type == 1 ? 10 : abs(latitude), # tilt = 10 deg for rooftop systems, abs(lat) for ground-mount
+    tilt::Real= array_type == 1 ? 10 : 25, # tilt = 10 deg for rooftop systems, 25 for ground-mount
     module_type::Int=0, # PV module type (0: Standard; 1: Premium; 2: Thin Film)
     losses::Real=0.14,
     azimuth::Real = latitude≥0 ? 180 : 0, # set azimuth to zero for southern hemisphere
@@ -130,7 +130,7 @@ struct PV <: AbstractTech
         off_grid_flag::Bool = false,
         latitude::Real,
         array_type::Int=1, # PV Watts array type (0: Ground Mount Fixed (Open Rack); 1: Rooftop, Fixed; 2: Ground Mount 1-Axis Tracking; 3 : 1-Axis Backtracking; 4: Ground Mount, 2-Axis Tracking)
-        tilt::Real= array_type == 1 ? 10 : abs(latitude), # tilt = 10 deg for rooftop systems, abs(lat) for ground-mount
+        tilt::Real= array_type == 1 ? 10 : 25, # tilt = 10 deg for rooftop systems, 25 for ground-mount
         module_type::Int=0, # PV module type (0: Standard; 1: Premium; 2: Thin Film)
         losses::Real=0.14,
         azimuth::Real = latitude≥0 ? 180 : 0, # set azimuth to zero for southern hemisphere
