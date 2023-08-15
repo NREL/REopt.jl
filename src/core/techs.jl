ba8406e282bfc8ada42c17a164235f708ad6282a
zolanaj <alexander.zolan@nrel.gov>
2023-08-15 17:20:32 -0600

    techs setup for electric heaters

--- src/core/techs.jl
+++ src/core/techs.jl
@@ -52,6 +52,7 @@ function Techs(p::REoptInputs, s::BAUScenario)
     absorption_chillers = String[]
     steam_turbines = String[]
     techs_can_supply_steam_turbine = String[]
+    electric_heaters = String[]
 
     if p.s.generator.existing_kw > 0
         push!(all_techs, "Generator")
@@ -94,7 +95,8 @@ function Techs(p::REoptInputs, s::BAUScenario)
         electric_chillers,
         absorption_chillers,
         steam_turbines,
-        techs_can_supply_steam_turbine        
+        techs_can_supply_steam_turbine,
+        electric_heaters        
     )
 end
 
@@ -127,7 +129,8 @@ function Techs(s::Scenario)
     electric_chillers = String[]
     absorption_chillers = String[]
     steam_turbines = String[]
-    techs_can_supply_steam_turbine = String[]    
+    techs_can_supply_steam_turbine = String[]
+    electric_heaters = String[]    
 
     if s.wind.max_kw > 0
         push!(all_techs, "Wind")
@@ -192,6 +195,13 @@ function Techs(s::Scenario)
         push!(steam_turbines, "SteamTurbine")
     end    
 
+    if !isnothing(s.electric_heater)
+        push!(all_techs, "ElectricHeater")
+        push!(heating_techs, "ElectricHeater")
+        push!(steam_turbines, "ElectricHeater")
+        push!(electric_heaters, "ElectricHeater")
+    end
+
     if s.settings.off_grid_flag
         append!(requiring_oper_res, pvtechs)
         append!(providing_oper_res, pvtechs)
@@ -221,7 +231,8 @@ function Techs(s::Scenario)
         electric_chillers,
         absorption_chillers,
         steam_turbines,
-        techs_can_supply_steam_turbine
+        techs_can_supply_steam_turbine,
+        electric_heaters
     )
 end
 
@@ -253,6 +264,7 @@ function Techs(s::MPCScenario)
         String[],
         String[],
         techs_no_turndown,
+        String[],
         String[],
         String[],
         String[],
