6fe87674d9ef671ca60865f922afcbd8cbae02f4
zolanaj <alexander.zolan@nrel.gov>
2023-08-22 15:20:09 -0600

    Add electric heater results

---
+++ src/results/electric_heater.jl
@@ -0,0 +1,89 @@
+# *********************************************************************************
+# REopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list
+# of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+#
+# Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+# *********************************************************************************
+"""
+`ElectricHeater` results keys:
+- `size_mmbtu_per_hour`  # Thermal production capacity size of the ElectricHeater [MMBtu/hr]
+- `electric_consumption_series_kw`  # Fuel consumption series [kW]
+- `annual_electric_consumption_kwh`  # Fuel consumed in a year [kWh]
+- `thermal_production_series_mmbtu_per_hour`  # Thermal energy production series [MMBtu/hr]
+- `annual_thermal_production_mmbtu`  # Thermal energy produced in a year [MMBtu]
+- `thermal_to_storage_series_mmbtu_per_hour`  # Thermal power production to TES (HotThermalStorage) series [MMBtu/hr]
+- `thermal_to_steamturbine_series_mmbtu_per_hour`  # Thermal power production to SteamTurbine series [MMBtu/hr]
+- `thermal_to_load_series_mmbtu_per_hour`  # Thermal power production to serve the heating load series [MMBtu/hr]
+
+!!! note "'Series' and 'Annual' energy outputs are average annual"
+	REopt performs load balances using average annual production values for technologies that include degradation. 
+	Therefore, all timeseries (`_series`) and `annual_` results should be interpretted as energy outputs averaged over the analysis period. 
+
+"""
+
+function add_electric_heater_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
+    r = Dict{String, Any}()
+    r["size_mmbtu_per_hour"] = round(value(m[Symbol("dvSize"*_n)]["ElectricHeater"]) / KWH_PER_MMBTU, digits=3)
+    @expression(m, ElectricHeaterElectricConsumptionSeries[ts in p.time_steps],
+        p.hours_per_time_step * sum(m[:dvThermalProduction][t,ts] / p.heating_cop[t] 
+        for t in p.techs.electric_heater))
+    r["electric_consumption_series_kw"] = round.(value.(ElectricHeaterElectricConsumptionSeries), digits=3)
+    r["annual_electric_consumption_kwh"] = round(sum(r["electric_consumption_series_kw"]), digits=3)
+
+	r["thermal_production_series_mmbtu_per_hour"] = 
+        round.(value.(m[:dvThermalProduction]["ElectricHeater", ts] for ts in p.time_steps) / KWH_PER_MMBTU, digits=5)
+	r["annual_thermal_production_mmbtu"] = round(sum(r["thermal_production_series_mmbtu_per_hour"]), digits=3)
+
+	if !isempty(p.s.storage.types.hot)
+        @expression(m, ElectricHeaterToHotTESKW[ts in p.time_steps],
+		    sum(m[:dvProductionToStorage][b,"ElectricHeater",ts] for b in p.s.storage.types.hot)
+            )
+    else
+        ElectricHeaterToHotTESKW = zeros(length(p.time_steps))
+    end
+	r["thermal_to_storage_series_mmbtu_per_hour"] = round.(value.(ElectricHeaterToHotTESKW / KWH_PER_MMBTU), digits=3)
+
+    if !isempty(p.techs.steam_turbine) && p.s.electric_heater.can_supply_steam_turbine
+        @expression(m, ElectricHeaterToSteamTurbine[ts in p.time_steps], m[:dvThermalToSteamTurbine]["ElectricHeater",ts])
+    else
+        ElectricHeaterToSteamTurbine = zeros(length(p.time_steps))
+    end
+    r["thermal_to_steamturbine_series_mmbtu_per_hour"] = round.(value.(ElectricHeaterToSteamTurbine), digits=3)
+
+	ElectricHeaterToLoad = @expression(m, [ts in p.time_steps],
+		m[:dvThermalProduction]["ElectricHeater", ts] - ElectricHeaterToHotTESKW[ts] - ElectricHeaterToSteamTurbine[ts]
+    )
+	r["thermal_to_load_series_mmbtu_per_hour"] = round.(value.(ElectricHeaterToLoad / KWH_PER_MMBTU), digits=3)
+
+    lifecycle_fuel_cost = p.pwf_fuel["ElectricHeater"] * value(
+        sum(m[:dvFuelUsage]["ElectricHeater", ts] * p.fuel_cost_per_kwh["ElectricHeater"][ts] for ts in p.time_steps)
+    )
+	r["lifecycle_fuel_cost_after_tax"] = round(lifecycle_fuel_cost * (1 - p.s.financial.offtaker_tax_rate_fraction), digits=3)
+	r["year_one_fuel_cost_before_tax"] = round(lifecycle_fuel_cost / p.pwf_fuel["ElectricHeater"], digits=3)
+
+    d["ElectricHeater"] = r
+	nothing
+end
