ff5d8aed1b4f47204eb2174d33cc0a204fa5013a
Rathod, Bhavesh <Bhavesh.Rathod@nrel.gov>
2023-09-26 19:16:13 -0400

    update results fields and add hybrid ghp test

--- src/constraints/ghp_constraints.jl
+++ src/constraints/ghp_constraints.jl
@@ -24,11 +24,9 @@ function add_ghp_constraints(m, p; _n="")
     m[:AvoidedCapexByGHP] = @expression(m,
         sum(p.avoided_capex_by_ghp_present_value[g] * m[Symbol("binGHP"*_n)][g] for g in p.ghp_options)
     )
-    @info "check1"
 
     m[:ResidualGHXCapCost] = @expression(m,
         sum(p.ghx_residual_value[g] * m[Symbol("binGHP"*_n)][g] for g in p.ghp_options)
     )
     
-    @info "check2"
 end
