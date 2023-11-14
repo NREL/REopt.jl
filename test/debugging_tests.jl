1bf6e716ece14a0f9ec2eb6d4d59a9143abdce36
hdunham <70401017+hdunham@users.noreply.github.com>
2023-11-14 15:59:30 -0700

    don't let AC coupled techs charge bess

--- test/debugging_tests.jl
+++ test/debugging_tests.jl
@@ -18,20 +18,20 @@ using DelimitedFiles
     @test results["ElectricStorage"]["dc_couple_inverter_size_kw"] ≈ 26.23 atol=0.1
 
     ground_pv = results["PV"][findfirst(pv -> pv["name"] == "ground", results["PV"])]
-    # roof_west = results["PV"][findfirst(pv -> pv["name"] == "roof_west", results["PV"])]
-    # roof_east = results["PV"][findfirst(pv -> pv["name"] == "roof_east", results["PV"])]
+    roof_west = results["PV"][findfirst(pv -> pv["name"] == "roof_west", results["PV"])]
+    roof_east = results["PV"][findfirst(pv -> pv["name"] == "roof_east", results["PV"])]
 
     @test ground_pv["size_kw"] ≈ 81.6667 atol=0.1
-    # @test roof_west["size_kw"] ≈ 30.0 atol=0.1
-    # @test roof_east["size_kw"] ≈ 10.0 atol=0.1
+    @test roof_west["size_kw"] ≈ 30.0 atol=0.1
+    @test roof_east["size_kw"] ≈ 10.0 atol=0.1
     @test ground_pv["lifecycle_om_cost_after_tax_bau"] ≈ 6256.0 atol=0.1
-    # @test roof_west["lifecycle_om_cost_after_tax_bau"] ≈ 4692.0 atol=0.1
+    @test roof_west["lifecycle_om_cost_after_tax_bau"] ≈ 4692.0 atol=0.1
     @test ground_pv["annual_energy_produced_kwh_bau"] ≈ 71463.73 atol=0.1
-    # @test roof_west["annual_energy_produced_kwh_bau"] ≈ 45938.58 atol=0.1
+    @test roof_west["annual_energy_produced_kwh_bau"] ≈ 45938.58 atol=0.1
     @test ground_pv["annual_energy_produced_kwh"] ≈ 145904.83 atol=0.1
-    # @test roof_west["annual_energy_produced_kwh"] ≈ 45938.58 atol=0.1
-    # @test roof_east["annual_energy_produced_kwh"] ≈ 16714.41 atol=0.1
-    @test sum(ground_pv["electric_to_storage_series_kw"]) ≈ 52393.351 atol=0.1
-    # @test sum(roof_west["electric_to_storage_series_kw"]) ≈ 0 atol=0.1
-    # @test sum(roof_east["electric_to_storage_series_kw"]) ≈ 0 atol=0.1
+    @test roof_west["annual_energy_produced_kwh"] ≈ 45938.58 atol=0.1
+    @test roof_east["annual_energy_produced_kwh"] ≈ 16714.41 atol=0.1
+    @test sum(ground_pv["electric_to_storage_series_kw"]) ≈ 52362.828 atol=0.1
+    @test sum(roof_west["electric_to_storage_series_kw"]) ≈ 0 atol=0.1
+    @test sum(roof_east["electric_to_storage_series_kw"]) ≈ 0 atol=0.1
 end
