0c18be6c688e529911a3115a0f698da93f52f932
hdunham <70401017+hdunham@users.noreply.github.com>
2024-01-12 12:19:47 -0700

    rm sum for b in storage.types.elec b/c b passed in

--- src/constraints/storage_constraints.jl
+++ src/constraints/storage_constraints.jl
@@ -136,7 +136,7 @@ function add_dc_coupled_tech_elec_storage_constraints(m, p, b; _n="")
         m[Symbol("dvDCCoupledTechStorageInverterSize"*_n)][b] >= 
         m[Symbol("dvDischargeFromStorage"*_n)][b,ts]
         + sum(p.production_factor[t, ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t,ts]
-        - sum(m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for b in p.s.storage.types.elec)
+        - m[Symbol("dvProductionToStorage"*_n)][b, t, ts]
         - m[Symbol("dvCurtail"*_n)][t, ts] for t in p.techs.dc_couple_with_stor) 
     )
 
