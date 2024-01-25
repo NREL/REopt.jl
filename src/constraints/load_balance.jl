7a6abbc9a72ae90dc7786509967a41f8f6249010
zolanaj <alexander.zolan@nrel.gov>
2024-01-24 21:16:37 -0700

    exclude ghp from COP-based electrical consumption in load balancing

--- src/constraints/load_balance.jl
+++ src/constraints/load_balance.jl
@@ -12,7 +12,7 @@ function add_elec_load_balance_constraints(m, p; _n="")
             sum(sum(m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for b in p.s.storage.types.elec) 
                 + m[Symbol("dvCurtail"*_n)][t, ts] for t in p.techs.elec)
             + sum(m[Symbol("dvGridToStorage"*_n)][b, ts] for b in p.s.storage.types.elec)
-            + sum(m[Symbol("dvCoolingProduction"*_n)][t, ts] / p.cop[t] for t in p.techs.cooling)
+            + sum(m[Symbol("dvCoolingProduction"*_n)][t, ts] / p.cop[t] for t in setdiff(p.techs.cooling,p.techs.ghp))
             + sum(m[Symbol("dvHeatingProduction"*_n)][t, q, ts] / p.heating_cop[t] for q in p.heating_loads, t in p.techs.electric_heater)
             + p.s.electric_load.loads_kw[ts]
             - p.s.cooling_load.loads_kw_thermal[ts] / p.cop["ExistingChiller"]
@@ -28,7 +28,7 @@ function add_elec_load_balance_constraints(m, p; _n="")
                 + sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t]) 
                 + m[Symbol("dvCurtail"*_n)][t, ts] for t in p.techs.elec)
             + sum(m[Symbol("dvGridToStorage"*_n)][b, ts] for b in p.s.storage.types.elec)
-            + sum(m[Symbol("dvCoolingProduction"*_n)][t, ts] / p.cop[t] for t in p.techs.cooling)
+            + sum(m[Symbol("dvCoolingProduction"*_n)][t, ts] / p.cop[t] for t in setdiff(p.techs.cooling,p.techs.ghp))
             + sum(m[Symbol("dvHeatingProduction"*_n)][t, q, ts] / p.heating_cop[t] for q in p.heating_loads, t in p.techs.electric_heater)
             + p.s.electric_load.loads_kw[ts]
             - p.s.cooling_load.loads_kw_thermal[ts] / p.cop["ExistingChiller"]
