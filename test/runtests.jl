2f4ca7409f109fdfe6067700ac0584970ff464ab
Zolan <azolan@nrel.gov>
2024-02-27 13:23:30 -0700

    update test values and tolerances for CHP Sizing
    - tighten CHP test values for LCC to match optimality gap
    - add more space to system sizing values

--- test/runtests.jl
+++ test/runtests.jl
@@ -1050,8 +1050,8 @@ else  # run HiGHS tests
                 m = Model(optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false, "log_to_console" => false, "mip_rel_gap" => 0.01))
                 results = run_reopt(m, inputs)
             
-                @test round(results["CHP"]["size_kw"], digits=0) ≈ 342.0 atol=1.0
-                @test round(results["Financial"]["lcc"], digits=0) ≈ 1.3476e7 atol=1.0e7
+                @test round(results["CHP"]["size_kw"], digits=0) ≈ 330.0 atol=20.0
+                @test round(results["Financial"]["lcc"], digits=0) ≈ 1.3476e7 rtol=1.0e-2
             end
         
             @testset "CHP Cost Curve and Min Allowable Size" begin
