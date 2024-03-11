94370ee2734376ab0db4aa49fb9b707660d8a82c
Zolan <azolan@nrel.gov>
2024-03-10 21:36:40 -0600

    add process heat to storage compatibility constraints

--- src/core/reopt.jl
+++ src/core/reopt.jl
@@ -231,6 +231,23 @@ function build_reopt!(m::JuMP.AbstractModel, p::REoptInputs)
 				@constraint(m, [ts in p.time_steps], m[:dvGridToStorage][b, ts] == 0)
 				@constraint(m, [t in p.techs.elec, ts in p.time_steps_with_grid],
 						m[:dvProductionToStorage][b, t, ts] == 0)
+			elseif b in p.s.storage.types.hot
+				@constraint(m, [q in q in setdiff(p.heating_loads, p.heating_loads_served_by_tes[b]), ts in p.time_steps], m[:dvHeatFromStorage][b,q,ts] == 0)
+				if "DomesticHotWater" in p.heating_loads_served_by_tes[b]
+					@constraint(m, [t in setdiff(p.heating_techs, p.techs_can_serve_dhw), ts in p.time_steps], m[:dvHeatToStorage][b,"DomesticHotWater",ts] == 0)
+				else
+					@constraint(m, [t in p.heating_techs, ts in p.time_steps], m[:dvHeatToStorage][b,"DomesticHotWater",ts] == 0)
+				end
+				if "SpaceHeating" in p.heating_loads_served_by_tes[b]
+					@constraint(m, [t in setdiff(p.heating_techs, p.techs_can_serve_space_heating), ts in p.time_steps], m[:dvHeatToStorage][b,"SpaceHeating",ts] == 0)
+				else
+					@constraint(m, [t in p.heating_techs, ts in p.time_steps], m[:dvHeatToStorage][b,"SpaceHeating",ts] == 0)
+				end
+				if "ProcessHeat" in p.heating_loads_served_by_tes[b]
+					@constraint(m, [t in setdiff(p.heating_techs, p.techs_can_serve_space_heating), ts in p.time_steps], m[:dvHeatToStorage][b,"ProcessHeat",ts] == 0)
+				else
+					@constraint(m, [t in p.heating_techs, ts in p.time_steps], m[:dvHeatToStorage][b,"ProcessHeat",ts] == 0)
+				end
 			end
 		else
 			add_storage_size_constraints(m, p, b)
