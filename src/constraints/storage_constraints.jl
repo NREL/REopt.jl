7e195558dd41a15f4edd24b31a8b0942d79c3b24
Rathod, Bhavesh <Bhavesh.Rathod@nrel.gov>
2024-03-15 13:36:39 -0400

    storage now serves EV loads
    changes for review

--- src/constraints/storage_constraints.jl
+++ src/constraints/storage_constraints.jl
@@ -40,10 +40,17 @@ function add_general_storage_dispatch_constraints(m, p, b; _n="")
         m[Symbol("dvStoredEnergy"*_n)][b, ts] >= p.s.storage.attr[b].soc_min_fraction * m[Symbol("dvStorageEnergy"*_n)][b]
     )
 
-    #Constraint (4j): Dispatch from storage is no greater than power capacity
-	@constraint(m, [ts in p.time_steps],
-        m[Symbol("dvStoragePower"*_n)][b] >= m[Symbol("dvDischargeFromStorage"*_n)][b, ts]
-    )
+    if !occursin("EV", b)
+        #Constraint (4?): Dispatch from elec storage to all EVs and other sinks is no greater than power capacity
+        @constraint(m, [ts in p.time_steps],
+            m[Symbol("dvStoragePower"*_n)][b] >= sum(m[Symbol("dvStorageToEV"*_n)][t, b, ts] + m[Symbol("dvDischargeFromStorage"*_n)][b, ts] for t in p.s.storage.types.ev)
+        )
+    else
+        #Constraint (4j): Dispatch from storage is no greater than power capacity
+        @constraint(m, [ts in p.time_steps],
+            m[Symbol("dvStoragePower"*_n)][b] >= m[Symbol("dvDischargeFromStorage"*_n)][b, ts]
+        )
+    end
 
 end
 
@@ -59,7 +66,7 @@ function add_elec_storage_dispatch_constraints(m, p, b; _n="")
             m[Symbol("dvStoredEnergy"*_n)][b, ts] == m[Symbol("dvStoredEnergy"*_n)][b, ts-1] + p.hours_per_time_step * (  
                 sum(p.s.storage.attr[b].charge_efficiency * m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for t in p.techs.elec)
                 + p.s.storage.attr[b].grid_charge_efficiency * m[Symbol("dvGridToStorage"*_n)][b, ts]
-                + sum(m[Symbol("dvStorageToEV"*_n)][b, t, ts] / p.s.storage.attr[b].discharge_efficiency for t in filter(x -> !occursin("EV", x), p.s.storage.types.elec))
+                + sum(m[Symbol("dvStorageToEV"*_n)][b, t, ts] / p.s.storage.attr[b].charge_efficiency for t in filter(x -> !occursin("EV", x), p.s.storage.types.elec))
                 - m[Symbol("dvDischargeFromStorage"*_n)][b,ts] / p.s.storage.attr[b].discharge_efficiency
                 - energy_drained_series[ts]
             )
--- src/results/electric_vehicle.jl
+++ src/results/electric_vehicle.jl
@@ -62,7 +62,10 @@ function add_electric_vehicle_results!(m::JuMP.AbstractModel, p::REoptInputs, d:
         end
     end
 
-    d[b]["on_site_techs_to_ev_series_kw"] = round.(value.(sum(m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for t in p.techs.elec) for ts in p.time_steps), digits=3)
+    d[b]["on_site_techs_to_ev_series_kw"] = round.(
+        value.(sum(m[Symbol("dvProductionToStorage")]["EV1", t, ts] for t in p.techs.elec) for ts in p.time_steps)
+        + value.(sum(m[Symbol("dvStorageToEV")]["EV1", t, ts] for t in filter(x -> !occursin("EV", x), p.s.storage.types.elec)) for ts in p.time_steps),
+    digits=3)
     d[b]["annual_on_site_techs_to_ev_charge_energy_kwh"] = round(sum(d[b]["on_site_techs_to_ev_series_kw"]), digits=1)
     d[b]["grid_to_ev_series_kw"] = round.(value.(m[Symbol("dvGridToStorage"*_n)][b, ts] for ts in p.time_steps), digits=3)
     d[b]["annual_grid_to_ev_charge_energy_kwh"] = round(sum(d[b]["grid_to_ev_series_kw"]), digits=1)
