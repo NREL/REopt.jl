5b525029f098c0ddbe4fdebed981faef065e3b28
Zolan <azolan@nrel.gov>
2024-03-21 21:33:39 -0600

    update max heat load to include process heat

--- src/core/scenario.jl
+++ src/core/scenario.jl
@@ -250,6 +250,7 @@ function Scenario(d::Dict; flex_hvac_from_json=false)
         process_heat_load = ProcessHeatLoad(; dictkeys_tosymbols(d["ProcessHeatLoad"])...,
             time_steps_per_hour=settings.time_steps_per_hour    
         )
+        max_heat_demand_kw += maximum(process_heat_load.loads_kw)
     else
         process_heat_load = ProcessHeatLoad(;
             heat_loads_mmbtu_per_hour=zeros(8760*settings.time_steps_per_hour),
