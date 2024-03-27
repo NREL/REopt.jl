c2daf5fa785479319379b2d53ee57aee4a93a58a
Hallie Dunham <hdunham@nrel.gov>
2024-03-27 12:06:00 -0600

    Update debugging_tests.jl

--- test/debugging_tests.jl
+++ test/debugging_tests.jl
@@ -6,58 +6,59 @@ using DelimitedFiles
 
 @testset "Solvers" begin
     
-    inputs = JSON.parsefile("./scenarios/res.json")
+    for scen in ["clean"]#"res", 
+        inputs = JSON.parsefile("./scenarios/"*scen*".json")
+        # Cbc
+        m1 = Model(optimizer_with_attributes(Cbc.Optimizer,
+            "seconds" => 420,
+            "ratioGap" => 0.001,
+            "logLevel" => 0)
+        )
+        m2 = Model(optimizer_with_attributes(Cbc.Optimizer,
+            "seconds" => 420,
+            "ratioGap" => 0.001,
+            "logLevel" => 0)
+        )
 
-    # # Cbc
-    # m1 = Model(optimizer_with_attributes(Cbc.Optimizer,
-    #     "seconds" => 420,
-    #     "ratioGap" => 0.001,
-    #     "logLevel" => 0)
-    # )
-    # m2 = Model(optimizer_with_attributes(Cbc.Optimizer,
-    #     "seconds" => 420,
-    #     "ratioGap" => 0.001,
-    #     "logLevel" => 0)
-    # )
+        results = run_reopt([m1,m2], inputs)
+        open(scen*"_Cbc_outputs.json","w") do f
+            JSON.print(f, results, 4)
+        end
 
-    # results = run_reopt([m1,m2], inputs)
-    # open("res_Cbc_outputs.json","w") do f
-    #     JSON.print(f, results, 4)
-    # end
+        # HiGHS
+        m1 = Model(optimizer_with_attributes(HiGHS.Optimizer, 
+            "time_limit" => 420.0,
+            "mip_rel_gap" => 0.001,
+            "output_flag" => false, 
+            "log_to_console" => false)
+        )
+        m2 = Model(optimizer_with_attributes(HiGHS.Optimizer,
+            "time_limit" => 420.0,
+            "mip_rel_gap" => 0.001,
+            "output_flag" => false, 
+            "log_to_console" => false)
+        )
 
-    # HiGHS
-    m1 = Model(optimizer_with_attributes(HiGHS.Optimizer, 
-        "time_limit" => 420.0,
-        "mip_rel_gap" => 0.001,
-        "output_flag" => false, 
-        "log_to_console" => false)
-    )
-    m2 = Model(optimizer_with_attributes(HiGHS.Optimizer,
-        "time_limit" => 420.0,
-        "mip_rel_gap" => 0.001,
-        "output_flag" => false, 
-        "log_to_console" => false)
-    )
+        results = run_reopt([m1,m2], inputs)
+        open(scen*"_HiGHS_outputs.json","w") do f
+            JSON.print(f, results, 4)
+        end
 
-    results = run_reopt([m1,m2], inputs)
-    open("res_HiGHS_outputs.json","w") do f
-        JSON.print(f, results, 4)
-    end
+        #SCIP
+        m1 = Model(optimizer_with_attributes(SCIP.Optimizer, 
+            "limits/time" => 420,
+            "limits/gap" => 0.001,
+            "display/verblevel" => 0)
+        )
+        m2 = Model(optimizer_with_attributes(SCIP.Optimizer, 
+            "limits/time" => 420,
+            "limits/gap" => 0.001,
+            "display/verblevel" => 0)
+        )
 
-    #SCIP
-    m1 = Model(optimizer_with_attributes(SCIP.Optimizer, 
-        "limits/time" => 420,
-        "limits/gap" => 0.001,
-        "display/verblevel" => 0)
-    )
-    m2 = Model(optimizer_with_attributes(SCIP.Optimizer, 
-        "limits/time" => 420,
-        "limits/gap" => 0.001,
-        "display/verblevel" => 0)
-    )
-
-    results = run_reopt([m1,m2], inputs)
-    open("res_SCIP_outputs.json","w") do f
-        JSON.print(f, results, 4)
+        results = run_reopt([m1,m2], inputs)
+        open(scen*"_SCIP_outputs.json","w") do f
+            JSON.print(f, results, 4)
+        end
     end
 end
