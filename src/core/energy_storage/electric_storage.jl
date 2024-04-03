00f2bf0e48ce09b8c141a8fa92c355722d498244
Hallie Dunham <hdunham@nrel.gov>
2024-04-03 12:22:40 -0600

    add option soc_min_applies_during_outages

--- src/core/energy_storage/electric_storage.jl
+++ src/core/energy_storage/electric_storage.jl
@@ -144,6 +144,7 @@ end
     inverter_efficiency_fraction::Float64 = 0.96
     rectifier_efficiency_fraction::Float64 = 0.96
     soc_min_fraction::Float64 = 0.2
+    soc_min_applies_during_outages::Bool = true
     soc_init_fraction::Float64 = off_grid_flag ? 1.0 : 0.5
     can_grid_charge::Bool = off_grid_flag ? false : true
     installed_cost_per_kw::Real = 910.0
@@ -177,6 +178,7 @@ Base.@kwdef struct ElectricStorageDefaults
     inverter_efficiency_fraction::Float64 = 0.96
     rectifier_efficiency_fraction::Float64 = 0.96
     soc_min_fraction::Float64 = 0.2
+    soc_min_applies_during_outages::Bool = true
     soc_init_fraction::Float64 = off_grid_flag ? 1.0 : 0.5
     can_grid_charge::Bool = off_grid_flag ? false : true
     installed_cost_per_kw::Real = 910.0
@@ -216,6 +218,7 @@ struct ElectricStorage <: AbstractElectricStorage
     inverter_efficiency_fraction::Float64
     rectifier_efficiency_fraction::Float64
     soc_min_fraction::Float64
+    soc_min_applies_during_outages::Bool
     soc_init_fraction::Float64
     can_grid_charge::Bool
     installed_cost_per_kw::Real
@@ -304,6 +307,7 @@ struct ElectricStorage <: AbstractElectricStorage
             s.inverter_efficiency_fraction,
             s.rectifier_efficiency_fraction,
             s.soc_min_fraction,
+            s.soc_min_applies_during_outages,
             s.soc_init_fraction,
             s.can_grid_charge,
             s.installed_cost_per_kw,
