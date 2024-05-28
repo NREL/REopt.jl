614a5126fa44ee641bf98847962087d541bed8a3
Bill Becker <William.Becker@nrel.gov>
2024-05-27 20:54:27 -0600

    Make separate load_type option for space_heating, dhw, and process_heat

--- src/core/simulated_load.jl
+++ src/core/simulated_load.jl
@@ -22,14 +22,13 @@ function simulated_load(d::Dict)
     load_type = get(d, "load_type", nothing)
 
     # Check consistency between type/length of doe_reference_name and percent_share (for blended/hybrid buildings)
-    doe_reference_name_input = get(d, "industry_reference_name", get(d, "doe_reference_name", nothing))
+    doe_reference_name_input = get(d, "doe_reference_name", nothing)
     percent_share_input = get(d, "percent_share", Real[])
     
     # Determine which set of valid names to use based on the origin of the input
-    if !isnothing(get(d, "industry_reference_name", nothing))
+    valid_names = default_buildings
+    if load_type == "process_heat"
         valid_names = default_process_types
-    else
-        valid_names = default_buildings
     end
     
     if !isnothing(doe_reference_name_input) && !(typeof(doe_reference_name_input) <: Vector{})
@@ -80,7 +79,7 @@ function simulated_load(d::Dict)
 
     if !isnothing(doe_reference_name)
         for drn in doe_reference_name
-            if !(drn in default_buildings)
+            if !(drn in valid_names)
                 throw(@error("Invalid doe_reference_name - $doe_reference_name. Select from the following: $default_buildings"))
             end
         end
@@ -98,7 +97,7 @@ function simulated_load(d::Dict)
         throw(@error("longitude $longitude is out of acceptable range (-180 <= longitude <= 180)"))
     end
 
-    if !(load_type in ["electric","heating","cooling"])
+    if !(load_type in ["electric","heating","cooling","space_heating","dhw","process_heat"])
         throw(@error("load_type parameter must be one of the following: 'electric', 'heating', or 'cooling'. If load_type is not specified, 'electric' is assumed."))
     end
 
@@ -398,6 +397,109 @@ function simulated_load(d::Dict)
     
         return response
     end
+
+    if load_type in ["space_heating", "dhw", "process_heat"]
+        error_list = []
+        for key in keys(d)
+            if occursin("_kw", key) || occursin("_ton", key)
+                append!(error_list, [key])
+            end
+        end
+        if !isempty(error_list)
+            throw(@error("Invalid key(s) $error_list for load_type=[space_heating, dhw, or process_heat"))
+        end
+        if isnothing(doe_reference_name)
+            throw(@error("Please supply a doe_reference_name and optional scaling parameters (annual_mmbtu or monthly_mmbtu)."))
+        end
+        # Annual loads (default is nothing)
+        annual_mmbtu = get(d, "annual_mmbtu", nothing)
+        # Monthly loads (default is empty list)
+        monthly_mmbtu = get(d, "monthly_mmbtu", Real[])
+        if !isempty(monthly_mmbtu)
+            if !(length(monthly_mmbtu) == 12)
+                throw(@error("monthly_mmbtu must contain a value for each of the 12 months"))
+            end
+            bad_index = []
+            for (i, mmbtu) in enumerate(monthly_mmbtu)
+                if isnothing(mmbtu)
+                    append!(bad_index, i)
+                end
+            end
+            if !isempty(bad_index)
+                throw(@error("monthly_mmbtu must contain a value for each month, and it is null for these months: $bad_index"))
+            end
+        end
+        # Addressable heating load (default is 1.0)
+        addressable_load_fraction = get(d, "addressable_load_fraction", 1.0)
+        if typeof(addressable_load_fraction) <: Vector{}
+            if !(length(addressable_load_fraction) == 12)
+                throw(@error("addressable_load_fraction must contain a value for each of the 12 months"))
+            end
+            bad_index = []
+            for (i, frac) in enumerate(addressable_load_fraction)
+                if isnothing(frac)
+                    append!(bad_index, i)
+                end
+            end
+            if length(bad_index) > 0
+                throw(@error("addressable_load_fraction must contain a value for each month, and it is null for these months: $bad_index"))
+            end
+        elseif addressable_load_fraction < 0.0 ||addressable_load_fraction > 1.0
+            throw(@error("addressable_load_fraction must be between 0.0 and 1.0"))
+        end
+
+        boiler_efficiency = get(d, "boiler_efficiency", EXISTING_BOILER_EFFICIENCY)
+        
+        heating_load_inputs = Dict{Symbol, Any}()
+        if load_type == "process_heat"
+            if length(doe_reference_name) > 1
+                heating_load_inputs[:blended_industry_reference_names] = doe_reference_name
+                heating_load_inputs[:blended_industry_reference_percents] = percent_share_list
+            else
+                heating_load_inputs[:industry_reference_name] = doe_reference_name[1]
+            end
+        else
+            if length(doe_reference_name) > 1
+                heating_load_inputs[:blended_doe_reference_names] = doe_reference_name
+                heating_load_inputs[:blended_doe_reference_percents] = percent_share_list                        
+            else
+                heating_load_inputs[:doe_reference_name] = doe_reference_name[1]
+            end
+        end
+        if addressable_load_fraction != 1.0
+            heating_load_inputs[:addressable_load_fraction] = addressable_load_fraction
+        end
+      
+        if load_type == "space_heating"
+            constructor = SpaceHeatingLoad
+        elseif load_type == "dhw"
+            constructor = DomesticHotWaterLoad
+        elseif load_type == "process_heat"
+            constructor = ProcessHeatLoad
+        end
+
+        heating_load = constructor(; heating_load_inputs...,
+                                    latitude=latitude, 
+                                    longitude=longitude,
+                                    annual_mmbtu=annual_mmbtu,
+                                    monthly_mmbtu=monthly_mmbtu,
+                                    existing_boiler_efficiency=boiler_efficiency
+                                )
+
+        load_series = heating_load.loads_kw ./ boiler_efficiency ./ KWH_PER_MMBTU
+        heating_monthly_energy = get_monthly_energy(load_series)
+    
+        response = Dict([
+            ("loads_mmbtu_per_hour", round.(load_series, digits=3)),
+            ("annual_mmbtu", round(heating_load.annual_mmbtu, digits=3)),
+            ("monthly_mmbtu", round.(heating_monthly_energy, digits=3)),
+            ("min_mmbtu_per_hour", round(minimum(load_series), digits=3)),
+            ("mean_mmbtu_per_hour", round(sum(load_series) / length(load_series), digits=3)),
+            ("max_mmbtu_per_hour", round(maximum(load_series), digits=3))
+        ])
+    
+        return response
+    end    
 
     if load_type == "cooling"
         error_list = []
