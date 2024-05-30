0285146d41df11a6c9da5eb910f991dd18f3ce1b
Zolan <azolan@nrel.gov>
2024-05-30 09:38:01 -0600

    change warning to throw an error if additional technology sets aren't sufficient to meet heating loads in optimal case and existing boiler is retired

--- src/core/reopt.jl
+++ src/core/reopt.jl
@@ -298,22 +298,16 @@ function build_reopt!(m::JuMP.AbstractModel, p::REoptInputs)
 
         # Zero out ExistingBoiler production if retire_in_optimal; length check avoids zeroing for BAU 
 		if !isnothing(p.s.existing_boiler) && p.s.existing_boiler.retire_in_optimal && length(p.techs.heating) > 1
-			can_retire = true
 			if !isnothing(p.s.dhw_load) && p.s.dhw_load.annual_mmbtu > 0 && isempty(setdiff(p.techs.can_serve_dhw, "ExistingBoiler"))
-				@warn "ExisitingBoiler.retire_in_optimal is true, but no other heating technologies can meet DomesticHotWater load.  ExistingBoiler will not be retired in the optimal case."
-				can_retire = false
+				@throw(@error("ExisitingBoiler.retire_in_optimal is true, but no other heating technologies can meet DomesticHotWater load."))
             end
 			if !isnothing(p.s.space_heating_load) && p.s.space_heating_load.annual_mmbtu > 0 && isempty(setdiff(p.techs.can_serve_space_heating, "ExistingBoiler"))
-				@warn "ExisitingBoiler.retire_in_optimal is true, but no other heating technologies can meet SpaceHeating load.  ExistingBoiler will not be retired in the optimal case."
-				can_retire = false
+				@throw(@error("ExisitingBoiler.retire_in_optimal is true, but no other heating technologies can meet SpaceHeating load."))
             end
 			if !isnothing(p.s.process_heat_load) && p.s.process_heat_load.annual_mmbtu > 0 && isempty(setdiff(p.techs.can_serve_process_heat, "ExistingBoiler"))
-				@warn "ExisitingBoiler.retire_in_optimal is true, but no other heating technologies can meet ProcessHeat load.  ExistingBoiler will not be retired in the optimal case."
-				can_retire = false
+				@throw(@error("ExisitingBoiler.retire_in_optimal is true, but no other heating technologies can meet ProcessHeat load."))
             end
-			if can_retire
-				no_existing_boiler_production(m, p)
-			end
+			no_existing_boiler_production(m, p)
         end
 		
 
