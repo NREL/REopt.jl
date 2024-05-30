51a7ff92864344a45d0ee5a52a7b9e86020ffdc5
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2024-05-30 13:09:56 -0600

    rm self discharge test now dupe

--- test/runtests.jl
+++ test/runtests.jl
@@ -552,32 +552,6 @@ else  # run HiGHS tests
             sum(results3["ElectricStorage"]["storage_to_load_series_kw"]) ≈ 15.382 atol=0.01 
     end
 
-    @testset "Electric Storage Self-Discharge" begin
-        model = Model(optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false, "log_to_console" => false))
-        data = JSON.parsefile("./scenarios/storage_om.json")
-
-        data["ElectricStorage"]["soc_init_fraction"] = 1.0
-        data["ElectricStorage"]["soc_min_fraction"] = 0.0
-        data["ElectricStorage"]["self_discharge_fraction_per_timestep"] = 0.0025/24
-
-        s = Scenario(data)
-        inputs = REoptInputs(s)
-        results_self_discharge = run_reopt(model, inputs)
-
-        model = Model(optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false, "log_to_console" => false))
-        data["ElectricStorage"]["self_discharge_fraction_per_timestep"] = 0.0
-
-        s = Scenario(data)
-        inputs = REoptInputs(s)
-        results_no_self_discharge = run_reopt(model, inputs)
-
-        @test results_self_discharge["ElectricStorage"]["year_one_om_cost_before_tax"] ≈ 0 atol=0.01
-        @test results_self_discharge["ElectricStorage"]["lifecycle_om_cost_after_tax"] ≈ 0 atol=0.01 
-        @test sum(results_no_self_discharge["ElectricStorage"]["storage_to_load_series_kw"]) - 
-              sum(results_self_discharge["ElectricStorage"]["storage_to_load_series_kw"]) ≈ 15.382 atol=0.01 
-       
-    end
-
     @testset "Imported Xpress Test Suite" begin
         @testset "Heating loads and addressable load fraction" begin
             # Default LargeOffice CRB with SpaceHeatingLoad and DomesticHotWaterLoad are served by ExistingBoiler
