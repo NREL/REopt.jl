c185bd75c9e97719d5916820ac2867ff823ea93a
An Pham <An.Pham@nrel.gov>
2024-06-04 07:49:39 -0600

    Update electric_storage.jl

--- src/core/energy_storage/electric_storage.jl
+++ src/core/energy_storage/electric_storage.jl
@@ -165,7 +165,7 @@ end
     model_degradation::Bool = false
     degradation::Dict = Dict()
     minimum_avg_soc_fraction::Float64 = 0.0
-    max_duration_hours::Real = 30.0 # Maximum amount of time storage can discharge at its rated power capacity (ratio of ElectricStorage size_kwh to size_kw)
+    max_duration_hours::Real = 9999.0 # Maximum amount of time storage can discharge at its rated power capacity (ratio of ElectricStorage size_kwh to size_kw)
     min_duration_hours::Real = 0.0 # Minimum amount of time storage can discharge at its rated power capacity
 ```
 """
