cfde553c8a0388ca951325318fdb8ea434183532
An Pham <An.Pham@nrel.gov>
2024-06-04 18:15:10 -0600

    print results for both ASHP and ASHP_WH

--- src/results/results.jl
+++ src/results/results.jl
@@ -105,9 +105,12 @@ function reopt_results(m::JuMP.AbstractModel, p::REoptInputs; _n="")
     end
 
     if !isempty(p.techs.ashp)
-        if "ASHP" in p.techs.ashp
+        if ("ASHP" in p.techs.ashp) && !("ASHP_WH" in p.techs.ashp)
             add_ashp_results(m, p, d; _n)
-        elseif "ASHP_WH" in p.techs.ashp
+        elseif ("ASHP_WH" in p.techs.ashp) && !("ASHP" in p.techs.ashp)
+            add_ashp_wh_results(m, p, d; _n)
+        elseif ("ASHP" in p.techs.ashp) && ("ASHP_WH" in p.techs.ashp)
+            add_ashp_results(m, p, d; _n)
             add_ashp_wh_results(m, p, d; _n)
         end
     end
