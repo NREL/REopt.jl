7028b4c8b84de41c07e916be268bd57ad7ff7001
Zolan <azolan@nrel.gov>
2024-06-13 14:31:04 -0600

    merge ASHP_WH as ASHP_WaterHeater

--- src/core/ashp.jl
+++ src/core/ashp.jl
@@ -104,6 +104,82 @@ function ASHP_SpaceHeater(;
 end
 
 
+"""
+ASHP Water Heater
+
+If a user provides the `ASHP_WaterHeater` key then the optimal scenario has the option to purchase 
+this new `ASHP_WaterHeater` to meet the domestic hot water load in addition to using the `ExistingBoiler`
+to meet the domestic hot water load. 
+
+```julia
+function ASHP_WaterHeater(;
+    min_ton::Real = 0.0, # Minimum thermal power size
+    max_ton::Real = BIG_NUMBER, # Maximum thermal power size
+    installed_cost_per_ton::Union{Real, nothing} = nothing, # Thermal power-based cost
+    om_cost_per_ton::Union{Real, nothing} = nothing, # Thermal power-based fixed O&M cost
+    macrs_option_years::Int = 0, # MACRS schedule for financial analysis. Set to zero to disable
+    macrs_bonus_fraction::Real = 0.0, # Fraction of upfront project costs to depreciate under MACRS
+    can_supply_steam_turbine::Union{Bool, nothing} = nothing # If the boiler can supply steam to the steam turbine for electric production
+    cop_heating::Array{<:Real,1}, # COP of the heating (i.e., thermal produced / electricity consumed)
+)
+```
+"""
+function ASHP_WaterHeater(;
+    min_ton::Real = 0.0,
+    max_ton::Real = BIG_NUMBER,
+    installed_cost_per_ton::Union{Real, Nothing} = nothing,
+    om_cost_per_ton::Union{Real, Nothing} = nothing,
+    macrs_option_years::Int = 0,
+    macrs_bonus_fraction::Real = 0.0,
+    cop_heating::Array{Float64,1} = Float64[],
+    cf_heating::Array{Float64,1} = Float64[]
+    )
+
+    defaults = get_ashp_defaults("DomesticHotWater")
+
+    # populate defaults as needed
+    if isnothing(installed_cost_per_ton)
+        installed_cost_per_ton = defaults["installed_cost_per_ton"]
+    end
+    if isnothing(om_cost_per_ton)
+        om_cost_per_ton = defaults["om_cost_per_ton"]
+    end
+
+     #pre-set defaults that aren't mutable due to technology specifications
+     can_supply_steam_turbine = defaults["can_supply_steam_turbine"]
+     can_serve_space_heating = defaults["can_serve_space_heating"]
+     can_serve_dhw = defaults["can_serve_dhw"]
+     can_serve_process_heat = defaults["can_serve_process_heat"]
+     can_serve_cooling = defaults["can_serve_cooling"]
+
+    # Convert max sizes, cost factors from mmbtu_per_hour to kw
+    min_kw = min_ton * KWH_THERMAL_PER_TONHOUR
+    max_kw = max_ton * KWH_THERMAL_PER_TONHOUR
+
+    installed_cost_per_kw = installed_cost_per_ton / KWH_THERMAL_PER_TONHOUR
+    om_cost_per_kw = om_cost_per_ton / KWH_THERMAL_PER_TONHOUR
+
+    
+    ASHP(
+        min_kw,
+        max_kw,
+        installed_cost_per_kw,
+        om_cost_per_kw,
+        macrs_option_years,
+        macrs_bonus_fraction,
+        can_supply_steam_turbine,
+        cop_heating,
+        Float64[],
+        cf_heating,
+        Float64[],
+        can_serve_dhw,
+        can_serve_space_heating,
+        can_serve_process_heat,
+        can_serve_cooling
+    )
+end
+
+
 
 """
 function get_ashp_defaults(load_served::String="SpaceHeating")
