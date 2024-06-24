f532d7fe9fb80a27b70905bb58e196457b1f7ce3
Zolan <azolan@nrel.gov>
2024-06-24 12:58:18 -0600

    rm unused results from ASHP docstrings

--- src/results/ashp.jl
+++ src/results/ashp.jl
@@ -1,19 +1,17 @@
 # REopt®, Copyright (c) Alliance for Sustainable Energy, LLC. See also https://github.com/NREL/REopt.jl/blob/master/LICENSE.
 
 """
-`ASHP` results keys:
+`ASHP_SpaceHeater` results keys:
 - `size_ton`  # Thermal production capacity size of the ASHP [ton/hr]
 - `electric_consumption_series_kw`  # Fuel consumption series [kW]
 - `annual_electric_consumption_kwh`  # Fuel consumed in a year [kWh]
 - `thermal_production_series_mmbtu_per_hour`  # Thermal heating energy production series [MMBtu/hr]
 - `annual_thermal_production_mmbtu`  # Thermal heating energy produced in a year [MMBtu]
 - `thermal_to_storage_series_mmbtu_per_hour`  # Thermal power production to TES (HotThermalStorage) series [MMBtu/hr]
-- `thermal_to_steamturbine_series_mmbtu_per_hour`  # Thermal power production to SteamTurbine series [MMBtu/hr]
 - `thermal_to_load_series_mmbtu_per_hour`  # Thermal power production to serve the heating load series [MMBtu/hr]
+- `thermal_to_space_heating_load_series_mmbtu_per_hour` # Thermal production to space heating load [MMBTU/hr]
 - `thermal_to_storage_series_ton` # Thermal production to ColdThermalStorage
 - `thermal_to_load_series_ton` # Thermal production to cooling load
-- `electric_consumption_series_kw`
-- `annual_electric_consumption_kwh`
 - `annual_thermal_production_tonhour` Thermal cooling energy produced in a year 
 
 
@@ -105,12 +103,7 @@ end
 - `thermal_production_series_mmbtu_per_hour`  # Thermal heating energy production series [MMBtu/hr]
 - `annual_thermal_production_mmbtu`  # Thermal heating energy produced in a year [MMBtu]
 - `thermal_to_storage_series_mmbtu_per_hour`  # Thermal power production to TES (HotThermalStorage) series [MMBtu/hr]
-- `thermal_to_steamturbine_series_mmbtu_per_hour`  # Thermal power production to SteamTurbine series [MMBtu/hr]
 - `thermal_to_load_series_mmbtu_per_hour`  # Thermal power production to serve the heating load series [MMBtu/hr]
-- `thermal_to_storage_series_ton` # Thermal production to ColdThermalStorage
-- `thermal_to_load_series_ton` # Thermal production to cooling load
-- `electric_consumption_series_kw`
-- `annual_electric_consumption_kwh`
 
 
 !!! note "'Series' and 'Annual' energy outputs are average annual"
