5c512b5db8b21bb92bb01663a8b71383b053d9ac
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2024-08-07 06:44:39 -0600

    remove final soc constraint

--- src/constraints/storage_constraints.jl
+++ src/constraints/storage_constraints.jl
@@ -36,9 +36,9 @@ function add_general_storage_dispatch_constraints(m, p, b; _n="")
         @constraint(m,
             m[Symbol("dvStoredEnergy"*_n)][b, 0] == p.s.storage.attr[b].soc_init_fraction * m[Symbol("dvStorageEnergy"*_n)][b]
         )
-        @constraint(m,
-            m[Symbol("dvStoredEnergy"*_n)][b, maximum(p.time_steps)] == p.s.storage.attr[b].soc_init_fraction * m[Symbol("dvStorageEnergy"*_n)][b]
-        )
+        # @constraint(m,
+        #     m[Symbol("dvStoredEnergy"*_n)][b, maximum(p.time_steps)] == p.s.storage.attr[b].soc_init_fraction * m[Symbol("dvStorageEnergy"*_n)][b]
+        # )
     end
 
     #Constraint (4n): State of charge upper bound is storage system size
