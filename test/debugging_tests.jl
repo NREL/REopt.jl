7b9655d4b201ac6c3c18e0002c6b4033bd490645
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2024-08-14 16:27:41 -0600

    Revert "make loads net in ac/dc test"
    This reverts commit edbed04ca79d0859c43d83beb6c7852ea5f15820.

--- test/debugging_tests.jl
+++ test/debugging_tests.jl
@@ -34,7 +34,7 @@ using DelimitedFiles
     @test ground_pv["annual_energy_produced_kwh"] ≈ 145904.83 atol=0.1
     @test roof_west["annual_energy_produced_kwh"] ≈ 45938.58 atol=0.1
     @test roof_east["annual_energy_produced_kwh"] ≈ 16714.41 atol=0.1
-    @test sum(ground_pv["electric_to_storage_series_kw"]) ≈ 63124.386 atol=0.1
+    @test sum(ground_pv["electric_to_storage_series_kw"]) ≈ 63160.44 atol=0.1
     @test sum(roof_west["electric_to_storage_series_kw"]) ≈ 0 atol=0.1
     @test sum(roof_east["electric_to_storage_series_kw"]) ≈ 0 atol=0.1
     expected_outage_results = Dict(
