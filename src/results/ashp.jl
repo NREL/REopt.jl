a98a645cdc703d3faa48f9cebcd5b6da34dd6fe5
Zolan <azolan@nrel.gov>
2024-08-22 08:45:11 -0600

    add CFs to ASHP tech results, COP to water heater

--- src/results/ashp.jl
+++ src/results/ashp.jl
@@ -83,18 +83,21 @@ function add_ashp_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
             p.hours_per_time_step * m[:dvCoolingProduction]["ASHP_SpaceHeater",ts] / p.cooling_cop["ASHP_SpaceHeater"][ts] 
         )
         r["cooling_cop"] = p.cooling_cop["ASHP_SpaceHeater"]
+        r["cooling_cf"] = p.cooling_cf["ASHP_SpaceHeater"]
     else
         r["thermal_to_storage_series_ton"] = zeros(length(p.time_steps))
         r["thermal_to_load_series_ton"] = zeros(length(p.time_steps))
         r["annual_thermal_production_tonhour"] = 0.0
         @expression(m, ASHPColdElectricConsumptionSeries, 0.0)
         r["cooling_cop"] = zeros(length(p.time_steps))
+        r["cooling_cf"] = zeros(length(p.time_steps))
     end
     r["electric_consumption_series_kw"] = round.(value.(ASHPElectricConsumptionSeries .+ ASHPColdElectricConsumptionSeries), digits=3)
     r["electric_consumption_for_cooling_series_kw"] = round.(value.(ASHPColdElectricConsumptionSeries), digits=3)
     r["electric_consumption_for_heating_series_kw"] = round.(value.(ASHPElectricConsumptionSeries), digits=3)
     r["annual_electric_consumption_kwh"] = p.hours_per_time_step * sum(r["electric_consumption_series_kw"])
     r["heating_cop"] = p.heating_cop["ASHP_SpaceHeater"]
+    r["heating_cf"] = p.heating_cf["ASHP_SpaceHeater"]
 
     d["ASHP_SpaceHeater"] = r
 	nothing
@@ -160,7 +163,9 @@ function add_ashp_wh_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n=
     
     r["electric_consumption_series_kw"] = round.(value.(ASHPWHElectricConsumptionSeries), digits=3)
     r["annual_electric_consumption_kwh"] = p.hours_per_time_step * sum(r["electric_consumption_series_kw"])
-
+    r["heating_cop"] = p.heating_cop["ASHP_SpaceHeater"]
+    r["heating_cf"] = p.heating_cf["ASHP_SpaceHeater"]
+    
     d["ASHP_WaterHeater"] = r
 	nothing
 end
