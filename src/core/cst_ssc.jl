241c6f15112ba5d765fd00bbb5faca6cfddc2e4d
Zolan <azolan@nrel.gov>
2024-09-13 16:15:05 -0600

    max of one for thermal, electrical factors (for message passing)

--- src/core/cst_ssc.jl
+++ src/core/cst_ssc.jl
@@ -235,8 +235,9 @@ function run_ssc(case_data::Dict)
         ### Free SSC
         @ccall hdl.ssc_module_free(ssc_module::Ptr{Cvoid})::Cvoid   
         @ccall hdl.ssc_data_free(data::Ptr{Cvoid})::Cvoid
-        R["thermal_production"] = thermal_production_norm
-        R["electric_consumption"] = electric_consumption_norm
+
+        R["thermal_production"] = min.(1,thermal_production_norm)
+        R["electric_consumption"] = min.(1,electric_consumption_norm)
         ### Check for errors
         if error == ""
             error = "No errors found."
