dc3ba22cebd8b38e5fda99dc6f1bdf71e15795f0
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2024-09-25 17:08:07 -0600

    Revert "Update expected Solar dataset test from updated NSRDB"
    This reverts commit d25ba18536121eb6576be2ddbbf21093e5407fe4.

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,7 +92,7 @@ else  # run HiGHS tests
             latitude, longitude = 65.0102196310875, 25.465387094897675
             radius = 0
             dataset, distance, datasource = REopt.call_solar_dataset_api(latitude, longitude, radius)
-            @test dataset ≈ "nsrdb"
+            @test dataset ≈ "intl"
 
             # 4. Fairbanks, AK 
             site = "Fairbanks"
