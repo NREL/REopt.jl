009038b23a71f8a5eb511a8a6de243204c646457
An Pham <An.Pham@nrel.gov>
2024-10-18 20:51:13 -0600

    Update runtests_cplex.jl

--- test/runtests_cplex.jl
+++ test/runtests_cplex.jl
@@ -23,7 +23,7 @@ elseif "CPLEX" in ARGS
     end
 
 else  # run HiGHS tests
-    @testset verbose=true "REopt test set using HiGHS solver" begin
+    @testset verbose=true "REopt test set using CPLEX solver" begin
         @testset "hybrid profile" begin
             electric_load = REopt.ElectricLoad(; 
                 blended_doe_reference_percents = [0.2, 0.2, 0.2, 0.2, 0.2],
