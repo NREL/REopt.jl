3955e954e5eaf743bb5e95bfeaf04e46696773eb
Bernal, Daniel <Daniel.Bernal@nrel.gov>
2024-11-27 15:36:01 -0700

    checked wind tests

--- src/core/wind.jl
+++ src/core/wind.jl
@@ -41,10 +41,12 @@
     If no `installed_cost_per_kw` is provided then it is determined from:
     ```julia
     size_class_to_installed_cost = Dict(
-        "residential"=> 6339.0,
-        "commercial"=> 4760.0,
-        "medium"=> 3137.0,
-        "large"=> 2386.0
+        "Bespoke 6 MW 196" => 2000,
+        "Bespoke 6 MW 170" => 2000,
+        "GE 1.5 MW" => 3000,
+        "Vestas V-47" => 4000,
+        "Northern Power Systems 100" => 4400,
+        "Bergey Excel 15" => 5000
     )
     ```
     If the `production_factor_series` is not provided then NREL's System Advisor Model (SAM) is used to get the wind turbine 
@@ -135,27 +137,35 @@ struct Wind <: AbstractTech
         operating_reserve_required_fraction::Real = off_grid_flag ? 0.50 : 0.0, # Only applicable when `off_grid_flag` is true. Applied to each time_step as a % of wind generation serving load.
         )
         size_class_to_hub_height = Dict(
-            "residential"=> 20,
-            "commercial"=> 40,
-            "medium"=> 60,  # Owen Roberts provided 50m for medium size_class, but Wind Toolkit has increments of 20m
-            "large"=> 80
+            "Bespoke 6 MW 196" => 140,
+            "Bespoke 6 MW 170" => 120, #supposed to be 115 but WINDToolkit has increments of 20 m
+            "GE 1.5 MW" => 80,
+            "Vestas V-47" => 60,
+            "Northern Power Systems 100" => 40, #supposed to be 37 but WINDToolkit has increments of 20 m
+            "Bergey Excel 15" => 20 #supposed to be 24 but WINDToolkit has increments of 20 m
         )
         size_class_to_installed_cost = Dict(
-            "residential"=> 6339.0,
-            "commercial"=> 4760.0,
-            "medium"=> 3137.0,
-            "large"=> 2386.0
+            "Bespoke 6 MW 196" => 2000,
+            "Bespoke 6 MW 170" => 2000,
+            "GE 1.5 MW" => 3000,
+            "Vestas V-47" => 4000,
+            "Northern Power Systems 100" => 4400,
+            "Bergey Excel 15" => 5000
         )
         
         if size_class == ""
-            if average_elec_load <= 12.5
-                size_class = "residential"
+            if average_elec_load <= 16
+                size_class = "Bergey Excel 15"
             elseif average_elec_load <= 100
-                size_class = "commercial"
-            elseif average_elec_load <= 1000
-                size_class = "medium"
+                size_class = "Northern Power Systems 100"
+            elseif average_elec_load <= 660
+                size_class = "Vestas V-47"
+            elseif average_elec_load <= 1500
+                size_class = "GE 1.5 MW"
+            elseif  average_elec_load <= 6000
+                size_class = "Bespoke 6 MW 170"
             else
-                size_class = "large"
+                size_class = "Bespoke 6 MW 196"
             end
         elseif !(size_class in keys(size_class_to_hub_height))
             throw(@error("Wind size_class must be one of $(keys(size_class_to_hub_height))"))
