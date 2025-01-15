34227241ea164fcb456e46d9cb4c305517994abf
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-01-14 20:20:54 -0700

    rm dupe constraint on dvDCCoupledTechStorageInverterSize

--- src/constraints/storage_constraints.jl
+++ src/constraints/storage_constraints.jl
@@ -131,14 +131,6 @@ function add_elec_storage_dispatch_constraints(m, p, b; _n="")
 end
 
 function add_dc_coupled_tech_elec_storage_constraints(m, p, b; _n="")
-	# Constraint (4d)-1: Lower bound on DC coupled PV and battery inverter power capacity
-    @constraint(m, [ts in p.time_steps],
-        m[Symbol("dvDCCoupledTechStorageInverterSize"*_n)][b] >= 
-        m[Symbol("dvDischargeFromStorage"*_n)][b,ts]
-        + sum(p.production_factor[t, ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t,ts]
-        - m[Symbol("dvProductionToStorage"*_n)][b, t, ts]
-        - m[Symbol("dvCurtail"*_n)][t, ts] for t in p.techs.dc_couple_with_stor) 
-    )
     # Constraint (4d)-1: Lower bounds on DC coupled PV and battery inverter power capacity 
     # (inverter direction)
     @constraint(m, [ts in p.time_steps],
