99197a323d6cf2d28b7bbf1dcda9b13ca39c21a6
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-01-14 17:02:06 -0700

    testing checkpoint after disallowing ac tech to charge dc coupled stor

--- test/debugging_tests.jl
+++ test/debugging_tests.jl
@@ -45,53 +45,53 @@ using DelimitedFiles
 # end
 
 @testset "AC and DC PVs" begin
-    # ## With single PV
-    # inputs = JSON.parsefile("./scenarios/ac_dc_pvs.json")
-    # inputs["ElectricLoad"]["loads_kw"] = 100 .* ones(8760)
-    # inputs["ElectricTariff"]["tou_energy_rates_per_kwh"] = repeat(cat(0.2*ones(15), 0.4*ones(5), 0.2*ones(4), dims=1), outer=365)
+    ## With single PV
+    inputs = JSON.parsefile("./scenarios/ac_dc_pvs.json")
+    inputs["ElectricLoad"]["loads_kw"] = 100 .* ones(8760)
+    inputs["ElectricTariff"]["tou_energy_rates_per_kwh"] = repeat(cat(0.2*ones(15), 0.4*ones(5), 0.2*ones(4), dims=1), outer=365)
 
-    # m = Model(optimizer_with_attributes(HiGHS.Optimizer))
-    # results = run_reopt(m, inputs)
-    # open("ac_dc_pvs_results_single_pv.json","w") do f
-    #     JSON.print(f, results, 4)
-    # end
-    # # results = JSON.parsefile("./ac_dc_pvs_results_single_pv.json")
+    m = Model(optimizer_with_attributes(HiGHS.Optimizer))
+    results = run_reopt(m, inputs)
+    open("ac_dc_pvs_results_single_pv.json","w") do f
+        JSON.print(f, results, 4)
+    end
+    # results = JSON.parsefile("./ac_dc_pvs_results_single_pv.json")
 
-    # results_baseline = JSON.parsefile("./ac_dc_pvs_results_baseline_single_pv.json")
+    results_baseline = JSON.parsefile("./ac_dc_pvs_results_baseline_single_pv.json")
 
-    # @test results["ElectricStorage"]["size_kw"] ≈ results_baseline["ElectricStorage"]["size_kw"] atol=1
-    # @test results["ElectricStorage"]["size_kwh"] ≈ results_baseline["ElectricStorage"]["size_kwh"] atol=1
-    # # @test results["ElectricStorage"]["dc_couple_inverter_size_kw"] ≈  atol=0.1
+    @test results["ElectricStorage"]["size_kw"] ≈ results_baseline["ElectricStorage"]["size_kw"] rtol=.005
+    @test results["ElectricStorage"]["size_kwh"] ≈ results_baseline["ElectricStorage"]["size_kwh"] rtol=.005
+    # @test results["ElectricStorage"]["dc_couple_inverter_size_kw"] ≈  atol=0.1
 
-    # pv_dc = results["PV"][findfirst(pv -> pv["name"] == "pv_dc", results["PV"])]
-    # pv_dc_baseline = results_baseline["PV"][findfirst(pv -> pv["name"] == "pv_dc", results_baseline["PV"])]
+    pv_dc = results["PV"][findfirst(pv -> pv["name"] == "pv_dc", results["PV"])]
+    pv_dc_baseline = results_baseline["PV"][findfirst(pv -> pv["name"] == "pv_dc", results_baseline["PV"])]
 
-    # @test pv_dc["size_kw"] ≈ pv_dc_baseline["size_kw"] atol=0.1
-    # @test pv_dc["annual_energy_produced_kwh"] ≈ pv_dc_baseline["annual_energy_produced_kwh"] atol=1
-    # @test sum(pv_dc["electric_to_storage_series_kw"]) ≈ sum(pv_dc_baseline["electric_to_storage_series_kw"]) atol=1
+    @test pv_dc["size_kw"] ≈ pv_dc_baseline["size_kw"] rtol=.005
+    @test pv_dc["annual_energy_produced_kwh"] ≈ pv_dc_baseline["annual_energy_produced_kwh"] rtol=.0001
+    @test sum(pv_dc["electric_to_storage_series_kw"]) ≈ sum(pv_dc_baseline["electric_to_storage_series_kw"]) rtol=.0001
 
-    # @test results["Financial"]["lcc"] ≈ results_baseline["Financial"]["lcc"] atol=1
+    @test results["Financial"]["lcc"] ≈ results_baseline["Financial"]["lcc"] rtol=.001
 
 
-    # ## With existing ac-coupled PV
-    # inputs = JSON.parsefile("./scenarios/ac_dc_pvs_existing.json")
-    # inputs["ElectricLoad"]["loads_kw"] = 100 .* ones(8760)
-    # inputs["ElectricTariff"]["tou_energy_rates_per_kwh"] = repeat(cat(0.2*ones(15), 0.4*ones(5), 0.2*ones(4), dims=1), outer=365)
+    ## With existing ac-coupled PV
+    inputs = JSON.parsefile("./scenarios/ac_dc_pvs_existing.json")
+    inputs["ElectricLoad"]["loads_kw"] = 100 .* ones(8760)
+    inputs["ElectricTariff"]["tou_energy_rates_per_kwh"] = repeat(cat(0.2*ones(15), 0.4*ones(5), 0.2*ones(4), dims=1), outer=365)
 
-    # m = Model(optimizer_with_attributes(HiGHS.Optimizer))
-    # results = run_reopt(m, inputs)
-    # open("ac_dc_pvs_results_existing.json","w") do f
-    #     JSON.print(f, results, 4)
-    # end
-    results = JSON.parsefile("./ac_dc_pvs_results_existing.json")
+    m = Model(optimizer_with_attributes(HiGHS.Optimizer))
+    results = run_reopt(m, inputs)
+    open("ac_dc_pvs_results_existing.json","w") do f
+        JSON.print(f, results, 4)
+    end
+    # results = JSON.parsefile("./ac_dc_pvs_results_existing.json")
 
     # switch comparison from can charge to cannot charge once ac coupled techs not allowed to chanrge dc coupled storage
     results_baseline_ac_can_charge = JSON.parsefile("./ac_dc_pvs_results_baseline_existing_can_charge.json")
-    results_baseline_ac_cannot_charge = JSON.parsefile("./ac_dc_pvs_results_baseline_existing_cannot_charge.json")
-    results_baseline = results_baseline_ac_can_charge
+    results_baseline_ac_cannot_charge = JSON.parsefile("./ac_dc_pvs_results_baseline_existing_cannot_charge.json") #existing is subtracted out of load and removed from PV inputs so sizes and o&m don't include it
+    results_baseline = results_baseline_ac_cannot_charge
     
-    @test results["ElectricStorage"]["size_kw"] ≈ results_baseline["ElectricStorage"]["size_kw"] atol=1
-    @test results["ElectricStorage"]["size_kwh"] ≈ results_baseline["ElectricStorage"]["size_kwh"] atol=1
+    @test results["ElectricStorage"]["size_kw"] ≈ results_baseline["ElectricStorage"]["size_kw"] rtol=.001
+    @test results["ElectricStorage"]["size_kwh"] ≈ results_baseline["ElectricStorage"]["size_kwh"] rtol=.001
     # @test results["ElectricStorage"]["dc_couple_inverter_size_kw"] ≈  atol=0.1
 
     pv_ac = results["PV"][findfirst(pv -> pv["name"] == "pv_ac", results["PV"])]
@@ -99,15 +99,11 @@ using DelimitedFiles
     pv_ac_baseline = results_baseline["PV"][findfirst(pv -> pv["name"] == "pv_ac", results_baseline["PV"])]
     pv_dc_baseline = results_baseline["PV"][findfirst(pv -> pv["name"] == "pv_dc", results_baseline["PV"])]
 
-    @test pv_ac["size_kw"] ≈ pv_ac_baseline["size_kw"] atol=0.1
-    @test pv_dc["size_kw"] ≈ pv_dc_baseline["size_kw"] atol=0.1
-    @test sum(pv_ac["electric_to_storage_series_kw"]) ≈ sum(pv_ac_baseline["electric_to_storage_series_kw"]) atol=.1
-    @test sum(pv_dc["electric_to_storage_series_kw"]) ≈ sum(pv_dc_baseline["electric_to_storage_series_kw"]) atol=1
-
-    # @test pv_ac["size_kw"] ≈ 30.0 atol=0.1
-    # @test pv_dc["size_kw"] ≈ pv_dc_baseline["size_kw"] + pv_ac_baseline["size_kw"] - 30.0 atol=0.1
-    # @test sum(pv_ac["electric_to_storage_series_kw"]) ≈ 0.0 atol=.1
-    # @test sum(pv_dc["electric_to_storage_series_kw"]) ≈ sum(pv_ac_baseline["electric_to_storage_series_kw"]) + sum(pv_dc_baseline["electric_to_storage_series_kw"]) atol=1
+    @test pv_ac["size_kw"] ≈ 30.0 rtol=.005
+    @test pv_dc["size_kw"] ≈ pv_dc_baseline["size_kw"] + pv_ac_baseline["size_kw"] rtol=.005
+    @test sum(pv_ac["electric_to_storage_series_kw"]) ≈ 0.0 rtol=.0001
+    @test sum(pv_dc["electric_to_storage_series_kw"]) ≈ sum(pv_ac_baseline["electric_to_storage_series_kw"]) + sum(pv_dc_baseline["electric_to_storage_series_kw"]) rtol=.0001
     
-    @test results["Financial"]["lcc"] ≈ results_baseline["Financial"]["lcc"] atol=1
+    expected_om_diff = results["Financial"]["lifecycle_om_costs_after_tax"] - results_baseline["Financial"]["lifecycle_om_costs_after_tax"]
+    @test results["Financial"]["lcc"] ≈ results_baseline["Financial"]["lcc"] + expected_om_diff rtol=.001
 end
