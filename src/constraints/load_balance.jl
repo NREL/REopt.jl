3b65bfb86cc67462db4efdce9b6d77529c5f99d3
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-01-14 20:48:47 -0700

    separate ac and dc techs in add_production_constraints

--- src/constraints/load_balance.jl
+++ src/constraints/load_balance.jl
@@ -92,12 +92,19 @@ function add_production_constraints(m, p; _n="")
             p.production_factor[t, ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t, ts]
         )
     else
-        @constraint(m, [t in p.techs.elec, ts in p.time_steps_with_grid],
-            sum(m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for b in p.s.storage.types.elec)  
-            + m[Symbol("dvCurtail"*_n)][t, ts]
-            + sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t])
+        @constraint(m, [t in p.techs.ac_coupled_with_storage, ts in p.time_steps_with_grid],
+            sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t])
             <= 
             p.production_factor[t, ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t, ts]
+            - sum(m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for b in p.s.storage.types.elec)  
+            - m[Symbol("dvCurtail"*_n)][t, ts]
+        )
+        @constraint(m, [t in p.techs.dc_coupled_with_storage, ts in p.time_steps_with_grid],
+            sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t])
+            <= 
+            p.production_factor[t, ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t, ts]
+            - sum(m[Symbol("dvProductionToStorage"*_n)][b, t, ts] for b in p.s.storage.types.elec)  
+            - m[Symbol("dvCurtail"*_n)][t, ts]
         )
     end
 
