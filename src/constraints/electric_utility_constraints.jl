9600ade4e63ccbe6e03cad4139d60dcddd9ff0b5
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-01-14 18:03:26 -0700

    separate ac and dc in dvProductionToGrid constraint

--- src/constraints/electric_utility_constraints.jl
+++ src/constraints/electric_utility_constraints.jl
@@ -2,10 +2,15 @@
 function add_export_constraints(m, p; _n="")
 
     ##Constraint (8e): Production export and curtailment no greater than production
-    @constraint(m, [t in p.techs.elec, ts in p.time_steps_with_grid],
-        p.production_factor[t,ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t,ts] 
-        >= sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t]) +
-           m[Symbol("dvCurtail"*_n)][t, ts]
+    @constraint(m, [t in p.techs.ac_coupled_with_storage, ts in p.time_steps_with_grid],
+        p.production_factor[t,ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t,ts] -
+        m[Symbol("dvCurtail"*_n)][t, ts]
+        >= sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t])
+    )
+    @constraint(m, [t in p.techs.dc_coupled_with_storage, ts in p.time_steps_with_grid],
+        p.production_factor[t,ts] * p.levelization_factor[t] * m[Symbol("dvRatedProduction"*_n)][t,ts] -
+        m[Symbol("dvCurtail"*_n)][t, ts]
+        >= sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for u in p.export_bins_by_tech[t])
     )
 
     binNEM = 0
