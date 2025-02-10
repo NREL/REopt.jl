0bc9ad7e968637ff101ad1edf9a88b7029ca0bb8
Zolan <azolan@nrel.gov>
2025-02-10 12:41:11 -0700

    change new results field to annual_electric_load_with_thermal_conversions_kwh

--- src/results/site.jl
+++ src/results/site.jl
@@ -48,10 +48,10 @@ function add_site_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
 	r["onsite_renewable_electricity_fraction_of_elec_load"] = round(value(m[:AnnualOnsiteREEleckWh])/value(m[:AnnualEleckWh]), digits=4)
 	r["onsite_and_grid_renewable_electricity_fraction_of_elec_load"] = round((value(m[:AnnualOnsiteREEleckWh]) + value(m[:AnnualGridREEleckWh])) /value(m[:AnnualEleckWh]), digits=4)
 	# This measure is the difference between all end-use electrical loads (including electrified heating and cooling) and the site-specific electrical load.
-	r["electric_load_converted_from_thermal_kwh"] = round(value(m[:AnnualEleckWh]) - (sum(p.s.electric_load.loads_kw .- p.s.cooling_load.loads_kw_thermal) * p.hours_per_time_step), digits=2)
 
 	# total renewable energy
 	add_re_tot_calcs(m,p)
+	r["annual_electric_load_with_thermal_conversions_kwh"] = round(value(m[:AnnualEleckWh]), digits=2)
 	r["onsite_renewable_energy_fraction_of_total_load"] = round(value(m[:AnnualOnsiteRETotkWh])/value(m[:AnnualTotkWh]), digits=4)
 	r["onsite_and_grid_renewable_energy_fraction_of_total_load"] = round((value(m[:AnnualOnsiteRETotkWh]) + value(m[:AnnualGridREEleckWh]))/value(m[:AnnualTotkWh]), digits=4)
 
