638f3c94e30d36a950aeccd91ab96ff484e1dae5
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-02-11 13:10:31 -0700

    create dc_coupled storage set

--- src/core/energy_storage/storage.jl
+++ src/core/energy_storage/storage.jl
@@ -16,6 +16,7 @@ mutable struct StorageTypes
     thermal::Vector{String}
     hot::Vector{String}
     cold::Vector{String}
+    dc_coupled::Vector{String}
 end
 ```
 """
@@ -25,10 +26,11 @@ mutable struct StorageTypes
     thermal::Vector{String}
     hot::Vector{String}
     cold::Vector{String}
-
+    dc_coupled::Vector{String}
 
     function StorageTypes()
         new(
+            String[],
             String[],
             String[],
             String[],
@@ -42,6 +44,7 @@ mutable struct StorageTypes
         elec_storage = String[]
         hot_storage = String[]
         cold_storage = String[]
+        dc_coupled = String[]
 
         for (k,v) in d
             if v.max_kw > 0.0 && v.max_kwh > 0.0
@@ -50,7 +53,9 @@ mutable struct StorageTypes
 
                 if typeof(v) <: AbstractElectricStorage
                     push!(elec_storage, k)
-
+                    if v.dc_coupled
+                        push!(dc_coupled, k)
+                    end
                 elseif typeof(v) <: HotThermalStorage
                     push!(hot_storage, k)
                 elseif typeof(v) <: ColdThermalStorage
@@ -68,7 +73,8 @@ mutable struct StorageTypes
             elec_storage,
             thermal_storage,
             hot_storage,
-            cold_storage
+            cold_storage,
+            dc_coupled
         )
     end
 end
