38164c9438bcab853b2421baf15d693ab77fb68e
Bill Becker <William.Becker@nrel.gov>
2025-02-18 21:42:12 -0700

    Add financial year_one fuel cost before and after tax

--- src/results/results.jl
+++ src/results/results.jl
@@ -111,6 +111,15 @@ function reopt_results(m::JuMP.AbstractModel, p::REoptInputs; _n="")
     if "ASHPWaterHeater" in p.techs.ashp_wh
         add_ashp_wh_results(m, p, d; _n)
     end
+
+    d["Financial"]["year_one_fuel_cost_before_tax"] = 0.0
+    d["Financial"]["year_one_fuel_cost_after_tax"] = 0.0
+    for tech in p.techs.fuel_burning
+        if tech in keys(d)
+            d["Financial"]["year_one_fuel_cost_before_tax"] += d[tech]["year_one_fuel_cost_before_tax"]
+            d["Financial"]["year_one_fuel_cost_after_tax"] += d[tech]["year_one_fuel_cost_after_tax"]
+        end
+    end
     
     return d
 end
@@ -130,6 +139,8 @@ function combine_results(p::REoptInputs, bau::Dict, opt::Dict, bau_scenario::BAU
         ("Financial", "lifecycle_om_costs_after_tax"),
         ("Financial", "year_one_om_costs_before_tax"),
         ("Financial", "year_one_om_costs_after_tax"),
+        ("Financial", "year_one_fuel_cost_before_tax"),
+        ("Financial", "year_one_fuel_cost_after_tax"),        
         ("Financial", "lifecycle_fuel_costs_after_tax"),
         ("Financial", "lifecycle_chp_standby_cost_after_tax"),
         ("Financial", "lifecycle_elecbill_after_tax"),
