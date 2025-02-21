80b8ebc54f09d14b879c8b15e6398d5f08577f45
Bill Becker <William.Becker@nrel.gov>
2025-02-21 10:28:09 -0700

    Report the correct size ExistingBoiler

--- src/results/existing_boiler.jl
+++ src/results/existing_boiler.jl
@@ -19,7 +19,9 @@
 """
 function add_existing_boiler_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
     r = Dict{String, Any}()
-    r["size_mmbtu_per_hour"] = round(value(m[Symbol("dvSize"*_n)]["ExistingBoiler"]) * p.s.existing_boiler.max_thermal_factor_on_peak_load / KWH_PER_MMBTU, digits=3)
+    report_size_mmbtu_per_hour = value(m[Symbol("dvSize"*_n)]["ExistingBoiler"]) / KWH_PER_MMBTU * p.s.existing_boiler.max_thermal_factor_on_peak_load
+    max_size_mmbtu_per_hour = p.s.existing_boiler.max_kw / KWH_PER_MMBTU
+    r["size_mmbtu_per_hour"] = round(min(report_size_mmbtu_per_hour, max_size_mmbtu_per_hour), digits=3)
 	r["fuel_consumption_series_mmbtu_per_hour"] = 
         round.(value.(m[:dvFuelUsage]["ExistingBoiler", ts] for ts in p.time_steps) ./ KWH_PER_MMBTU, digits=5)
     r["annual_fuel_consumption_mmbtu"] = round(sum(r["fuel_consumption_series_mmbtu_per_hour"]), digits=5)
