5309d049a535b2c7ff76814855a6c41b7a88ed99
Zolan <azolan@nrel.gov>
2025-02-24 13:47:10 -0700

    generalize import/export constraint name for multinode

--- src/constraints/electric_utility_constraints.jl
+++ src/constraints/electric_utility_constraints.jl
@@ -2,7 +2,7 @@
 function add_export_constraints(m, p; _n="")
 
     ## Imports and Exports must be no greater than the transmission limit
-    @constraint(m, ImportExportLimitCon[ts in p.time_steps_with_grid],
+    m[Symbol("ImportExportLimitCon"*_n)] = @constraint(m, [ts in p.time_steps_with_grid],
     sum(m[Symbol("dvProductionToGrid"*_n)][t, u, ts] for t in p.techs.elec, u in p.export_bins_by_tech[t])
         + sum(sum( m[Symbol("dvGridPurchase"*_n)][ts, tier] for tier in 1:p.s.electric_tariff.n_energy_tiers))
         <= p.s.electric_utility.transmission_limit_kw
