5f597c8c391b958dc5e1d2125c6864c691a9109c
Kadlec <jkadlec@nrel.gov>
2025-07-08 08:12:15 -0600

    adding in email to simple test file

--- test/simple_test.jl
+++ test/simple_test.jl
@@ -1,12 +1,13 @@
 using JSON, REopt, HiGHS, JuMP
 
 ENV["NREL_DEVELOPER_API_KEY"]="X52RT85w3q8uXkGqAJun7FUQfnWAmW4i1ypRdLle"
+ENV["NREL_DEVELOPER_EMAIL"]="jaret.kadlec@nrel.gov"
 
 m1 = Model(HiGHS.Optimizer)
 m2 = Model(HiGHS.Optimizer)
-results = run_reopt([m1,m2], "./scenarios/post_1.json")
+results = run_reopt([m1,m2], "./scenarios/pv_cst_tes.json")
 
 # Save results to JSON
-open("./outputs/post_1.json","w") do f
+open("./outputs/cst_outputs.json","w") do f
     JSON.print(f, results, 2)
 end
