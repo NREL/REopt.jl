760a0f6cc2606d3045f67f89d58893f88fde8e8b
Zolan <azolan@nrel.gov>
2025-08-29 08:03:51 -0600

    add HotThermalStorage to CST test to confirm two hot storage techs are compatible

--- test/runtests.jl
+++ test/runtests.jl
@@ -3363,13 +3363,14 @@ else  # run HiGHS tests
             d = JSON.parsefile("./scenarios/cst.json")
             s = Scenario(d)
             p = REoptInputs(s)
-            m1 = Model(optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false, "mip_rel_gap" => 0.01))
+            m1 = Model(optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false, "log_to_console" => false, "mip_rel_gap" => 0.01))
             results = run_reopt(m1, p)
             @test results["CST"]["annual_thermal_production_mmbtu"] ≈ 272.49 atol=0.1
             @test results["CST"]["size_kw"] ≈ 100.0 atol=0.001
             @test results["CST"]["size_mmbtu_per_hour"] ≈ 100.0 / REopt.KWH_PER_MMBTU atol=1.0e-3
-            @test results["ExistingBoiler"]["annual_thermal_production_mmbtu"] ≈ 717087.35 atol=0.1
+            @test results["ExistingBoiler"]["annual_thermal_production_mmbtu"] ≈ 717087.86 rtol=1.0e-4
             @test results["HighTempThermalStorage"]["size_kwh"] ≈ 10000.0 atol=0.1
+            @test results["HotThermalStorage"]["size_gal"] ≈ 1000.0 atol=0.1
         end
 
         @testset "Custom REopt logger" begin
