cc5eeba4625df788be8061666c928a6ff54a91bf
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-09-04 13:28:27 -0600

    make sector, federal_procurement_type named args in GHP()

--- src/core/ghp.jl
+++ src/core/ghp.jl
@@ -131,7 +131,7 @@ Base.@kwdef mutable struct GHP <: AbstractGHP
 end
 
 
-function GHP(response::Dict, d::Dict, sector::String, federal_procurement_type::String)
+function GHP(response::Dict, d::Dict; sector::String, federal_procurement_type::String)
     set_tech_sector_defaults(d; sector=sector, federal_procurement_type=federal_procurement_type)
 
     ghp = GHP(; ghpghx_response = response, dictkeys_tosymbols(d)...)
