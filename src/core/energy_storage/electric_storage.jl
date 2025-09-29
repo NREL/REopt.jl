0f977f71ade80c806f74dad2173e8aad90977927
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-09-29 17:04:10 -0600

    update sector defaults utils usage in stor stucts
    and update docstrings

--- src/core/energy_storage/electric_storage.jl
+++ src/core/energy_storage/electric_storage.jl
@@ -186,10 +186,10 @@ end
     battery_replacement_year::Int = 10
     cost_constant_replacement_year::Int = 10
     om_cost_fraction_of_installed_cost::Float64 = 0.025 # Annual O&M cost as a fraction of installed cost
-    macrs_option_years::Int = get(get_sector_defaults_storage(; sector=sector, federal_procurement_type=federal_procurement_type), "macrs_option_years", 5)
-    macrs_bonus_fraction::Float64 = get(get_sector_defaults_storage(; sector=sector, federal_procurement_type=federal_procurement_type), "macrs_bonus_fraction", 1.0)
+    macrs_option_years::Int = 5 #Note: default may change if Site.sector is not "commercial/industrial"
+    macrs_bonus_fraction::Float64 = 1.0 #Note: default may change if Site.sector is not "commercial/industrial"
     macrs_itc_reduction::Float64 = 0.5
-    total_itc_fraction::Float64 = get(get_sector_defaults_storage(; sector=sector, federal_procurement_type=federal_procurement_type), "total_itc_fraction", 0.3)
+    total_itc_fraction::Float64 = 0.3 #Note: default may change if Site.sector is not "commercial/industrial"
     total_rebate_per_kw::Real = 0.0
     total_rebate_per_kwh::Real = 0.0
     charge_efficiency::Float64 = rectifier_efficiency_fraction * internal_efficiency_fraction^0.5
@@ -292,7 +292,7 @@ struct ElectricStorage <: AbstractElectricStorage
     max_duration_hours::Real
 
     function ElectricStorage(d::Dict, f::Financial, s::Site)  
-        set_storage_sector_defaults!(d; sector=s.sector, federal_procurement_type=s.federal_procurement_type)
+        set_sector_defaults!(d; struct_name="Storage", sector=s.sector, federal_procurement_type=s.federal_procurement_type)
 
         s = ElectricStorageDefaults(;dictkeys_tosymbols(d)...)
 
