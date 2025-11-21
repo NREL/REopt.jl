13881aa26edb85c6f3ffd1c9941a916561afe031
Rathod, Bhavesh <Bhavesh.Rathod@nrel.gov>
2025-11-21 14:02:22 -0500

    Update electric_tariff.jl
    Fix documentation

--- src/results/electric_tariff.jl
+++ src/results/electric_tariff.jl
@@ -15,40 +15,40 @@
 - `lifecycle_coincident_peak_cost_after_tax` lifecycle coincident peak charge in present value, after tax
 - `year_one_coincident_peak_cost_before_tax` coincident peak charge over the first year
 
-Outputs related to electric tariff (year-one rates and costs; not escalated):
-- `monthly_fixed_cost_series_before_tax` the fixed monthly cost of electricity for modeled meter per chosen electric tariff in \$/month
-- `energy_rate_series` dictionary for cost of electricity, each key corresponds to a tier with value being \$/kWh timeseries
+Outputs related to electric tariff (year-one rates and costs not escalated):
+- `monthly_fixed_cost_series_before_tax` the fixed monthly cost of electricity for modeled meter per chosen electric tariff in dollars/month
+- `energy_rate_series` dictionary for cost of electricity, each key corresponds to a tier with value being dollars/kWh timeseries
 - `energy_rate_tier_limits` dictionary for energy rate tier limits, each key corresponds to a tier with value being kWh limit
-- `energy_rate_average_series` average energy rate across all tiers as \$/kWh timeseries
-- `facility_demand_monthly_rate_series` facility demand charge in \$/kW (keys = tiers, values = demand charge for each month)
+- `energy_rate_average_series` average energy rate across all tiers as dollars/kWh timeseries
+- `facility_demand_monthly_rate_series` facility demand charge in dollars/kW (keys = tiers, values = demand charge for each month)
 - `facility_demand_monthly_rate_tier_limits` facility demand charge limits in kW (keys = tiers, values = demand limit for each month)
-- `tou_demand_rate_series` is a dictionary with TOU demand charges in \$/kW as timeseries for each timestep
-- `demand_rate_average_series` average TOU demand rate across all tiers as \$/kW timeseries
+- `tou_demand_rate_series` is a dictionary with TOU demand charges in dollars/kW as timeseries for each timestep
+- `demand_rate_average_series` average TOU demand rate across all tiers as dollars/kW timeseries
 - `tou_demand_rate_tier_limits` TOU demand charge limits [kW]
 
-Outputs related to REopt calculated costs of electricity (year-one rates and costs; not escalated):
-- `energy_cost_series_before_tax` timeseries of cost of electricity purchases from the grid (grid to total net load) [\$]
-- `monthly_energy_cost_series_before_tax` Monthly energy costs, summed across all tiers [\$]
-- `monthly_facility_demand_cost_series_before_tax`  Monthly facility demand cost, dictionary by Tier number [\$]
+Outputs related to REopt calculated costs of electricity (year-one rates and costs not escalated):
+- `energy_cost_series_before_tax` timeseries of cost of electricity purchases from the grid (grid to total net load) [dollars]
+- `monthly_energy_cost_series_before_tax` Monthly energy costs, summed across all tiers [dollars]
+- `monthly_facility_demand_cost_series_before_tax`  Monthly facility demand cost, dictionary by Tier number [dollars]
 - `tou_demand_metrics` -> month: Month this TOU period applies to
 - `tou_demand_metrics` -> tier: Tier of TOU period
-- `tou_demand_metrics` -> demand_rate: \$/kW TOU demand charge
+- `tou_demand_metrics` -> demand_rate: dollars/kW TOU demand charge
 - `tou_demand_metrics` -> measured_tou_peak_demand: measured peak kW load in TOU period [kW]
-- `tou_demand_metrics` -> demand_charge_before_tax`: calculated demand charge [\$]
-- `monthly_tou_demand_cost_series_before_tax`  Monthly TOU demand costs, dictionary by Tier number [\$]
-- `monthly_demand_cost_series_before_tax` Monthly total facility plus TOU demand costs, summed across all tiers [\$]
+- `tou_demand_metrics` -> demand_charge_before_tax`: calculated demand charge [dollars]
+- `monthly_tou_demand_cost_series_before_tax`  Monthly TOU demand costs, dictionary by Tier number [dollars]
+- `monthly_demand_cost_series_before_tax` Monthly total facility plus TOU demand costs, summed across all tiers [dollars]
 
 Prefix net_metering, wholesale, or net_metering_excess (export categories) for following outputs, all can be in results if relevant inputs are provided.
-- `_export_rate_series` export rate timeseries for type of export category in [\$/kWh]
+- `_export_rate_series` export rate timeseries for type of export category in [dollars/kWh]
 - `_electric_to_grid_series_kw` exported electricity timeseries for type of export category in [kW]
 - `_monthly_export_series_kwh` monthly exported energy totals by export category in [kWh]
-- `_monthly_export_cost_benefit_before_tax` monthly export benefit by export category in [\$]
+- `_monthly_export_cost_benefit_before_tax` monthly export benefit by export category in [dollars]
 
 !!! note "Handling of tiered rates"
 	Energy and demand charges costs are returned as a dictionary with each key corresponding to a cost tier. 
     REopt assumes all TOU periods have the same tier limits
-"""
 
+"""
 function add_electric_tariff_results(m::JuMP.AbstractModel, p::REoptInputs, d::Dict; _n="")
     # Adds the `ElectricTariff` results to the dictionary passed back from `run_reopt` using the solved model `m` and the `REoptInputs` for node `_n`.
     # Note: the node number is an empty string if evaluating a single `Site`.
